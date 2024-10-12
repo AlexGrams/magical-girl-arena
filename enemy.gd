e3404ae47868d6470d251762892b778b56035460
Victoria W <56736494+Tori-W@users.noreply.github.com>
2024-10-12 02:00:50 -0700

    Enemy health and taking damage anim

--- enemy.gd
+++ enemy.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var player:Node2D
 var speed:float = 100
+var max_health = 100
+var health = 100
 @onready var exp_scene = preload("res://exp_orb.tscn")
 
 func _ready() -> void:
@@ -10,9 +12,19 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if player != null:
 		global_position = global_position.move_toward(player.global_position, delta*speed)
-	
-func _on_area_2d_area_entered(area: Area2D) -> void:
+
+func take_damage(damage: float) -> void:
+	health -= damage
+	$AnimationPlayer.play("take_damage")
+	if health <= 0:
+		die()
+
+func die() -> void:
 	var exp_orb = exp_scene.instantiate()
 	exp_orb.global_position = global_position
 	get_tree().root.call_deferred("add_child", exp_orb)
 	queue_free()
+	
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area is BulletHitbox:
+		take_damage(area.damage)
