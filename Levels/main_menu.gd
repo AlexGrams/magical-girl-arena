822735f7ff7ccef97e0bd087e1d28fc43cfcdba5
Alexander Grams <alexgrams222@gmail.com>
2024-12-16 02:00:08 -0800

    Test RPC is activated on client and server after a client connects

--- Levels/main_menu.gd
+++ Levels/main_menu.gd
@@ -2,15 +2,18 @@ extends Node2D
 
 @export var start_game_scene: PackedScene
 
+var timer = -1.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-func _on_host_button_button_down() -> void:
-	print("Hosting")
-	#get_tree().change_scene_to_packed(start_game_scene)
-
+func _process(delta: float) -> void:
+	if timer >= 0.0:
+		timer += delta
+		if timer >= 1.0:
+			MultiplayerManager.test_rpc.rpc()
+			timer = -1.0
 
 func _on_quit_button_button_down() -> void:
 	get_tree().quit()
@@ -21,5 +24,11 @@ func _on_lobby_button_button_down() -> void:
 	$Lobby.visible = true
 
 
+func _on_host_button_button_down() -> void:
+	MultiplayerManager.create_server()
+	#get_tree().change_scene_to_packed(start_game_scene)
+
+
 func _on_join_button_button_down() -> void:
-	print("Joining (as peer)")
+	MultiplayerManager.create_client()
+	timer = 0.0
--- Multiplayer/multiplayer_manager.gd
+++ Multiplayer/multiplayer_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+class_name MGAMultiplayerManager
+
 # Port number for ENet multiplayer connections. Not commonly used or assigned:
 # https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
 const PORT_NUMBER: int = 34229
@@ -18,20 +20,20 @@ signal host_created()
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 	
-
 # Create the server for the multiplayer game
 func create_server():
 	peer = ENetMultiplayerPeer.new()
 	var create_server_result = peer.create_server(PORT_NUMBER, MAX_PLAYERS)
 	multiplayer.multiplayer_peer = peer
 	
+	print(str(create_server_result))
+	
 	host_created.emit()
-
+	print("Giga hosting!")
 
 # Create a client for the multiplayer game
 func create_client():
@@ -39,4 +41,13 @@ func create_client():
 	# NOTE: For testing, I'll only be trying to connect to the localhost
 	var create_client_result = peer.create_client("localhost", PORT_NUMBER)
 	multiplayer.multiplayer_peer = peer
+	
+	print(create_client_result)
+	
 	peer_created.emit()
+	print("Clienting!")
+
+# Test function to see if server connection is working.
+@rpc("any_peer", "call_local", "reliable")
+func test_rpc():
+	print("RPC has fired!")
