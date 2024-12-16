07ec8f62e595e082fcddf12e116ab48b4e87965a
Alexander Grams <alexgrams222@gmail.com>
2024-12-16 00:49:30 -0800

    "Host" and "Join" buttons on the Lobby menu.
    Will try to get ENet working first, then switch to Steam multiplayer. It is harder to get Steam working because it requires multiple accounts and computers.

--- Levels/main_menu.gd
+++ Levels/main_menu.gd
@@ -7,8 +7,9 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-func _on_start_button_button_down() -> void:
-	get_tree().change_scene_to_packed(start_game_scene)
+func _on_host_button_button_down() -> void:
+	print("Hosting")
+	#get_tree().change_scene_to_packed(start_game_scene)
 
 
 func _on_quit_button_button_down() -> void:
@@ -18,3 +19,7 @@ func _on_quit_button_button_down() -> void:
 func _on_lobby_button_button_down() -> void:
 	$Main.visible = false
 	$Lobby.visible = true
+
+
+func _on_join_button_button_down() -> void:
+	print("Joining (as peer)")
