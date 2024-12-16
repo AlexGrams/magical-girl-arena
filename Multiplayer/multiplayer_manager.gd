abd52818b5c9f2bd849cb9f385d175206727fdfb
Alexander Grams <alexgrams222@gmail.com>
2024-12-16 01:23:31 -0800

    Created multiplayer controller script and basic client and server creation functions

---
+++ Multiplayer/multiplayer_manager.gd
@@ -0,0 +1,42 @@
+extends Node
+
+# Port number for ENet multiplayer connections. Not commonly used or assigned:
+# https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
+const PORT_NUMBER: int = 34229
+
+# Max number of players. I believe this includes the server.
+const MAX_PLAYERS: int = 4
+
+var peer: MultiplayerPeer = null
+
+# Emitted after a peer is created
+signal peer_created()
+# Emitted after a host is created
+signal host_created()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+# Create the server for the multiplayer game
+func create_server():
+	peer = ENetMultiplayerPeer.new()
+	var create_server_result = peer.create_server(PORT_NUMBER, MAX_PLAYERS)
+	multiplayer.multiplayer_peer = peer
+	
+	host_created.emit()
+
+
+# Create a client for the multiplayer game
+func create_client():
+	peer = ENetMultiplayerPeer.new()
+	# NOTE: For testing, I'll only be trying to connect to the localhost
+	var create_client_result = peer.create_client("localhost", PORT_NUMBER)
+	multiplayer.multiplayer_peer = peer
+	peer_created.emit()
