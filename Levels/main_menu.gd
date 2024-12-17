846fb008c5edbfbb9b5fdf3c2dddcb78adda13dc
Alexander Grams <alexgrams222@gmail.com>
2024-12-16 23:48:28 -0800

    Load the game scene when a player connects.
    Temporary for testing. Will make a button for starting the game later.

--- Levels/main_menu.gd
+++ Levels/main_menu.gd
@@ -3,13 +3,19 @@ extends Node2D
 @export var start_game_scene: PackedScene
 
 var timer = -1.0
+# Map of connected players to their data
+var player_ids = {}
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	multiplayer.peer_connected.connect(_on_peer_connected)
+	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+
 
 func _process(_delta: float) -> void:
 	pass
+
 
 func _on_quit_button_button_down() -> void:
 	get_tree().quit()
@@ -22,18 +28,24 @@ func _on_lobby_button_button_down() -> void:
 
 func _on_host_button_button_down() -> void:
 	MultiplayerManager.create_server()
-	#get_tree().change_scene_to_packed(start_game_scene)
-	
-	multiplayer.multiplayer_peer.peer_connected.connect(on_peer_connected)
 
 
 func _on_join_button_button_down() -> void:
 	MultiplayerManager.create_client()
 
-func on_peer_connected(id: int) -> void:
-	say_something.rpc()
 
-# Test function to see if server connection is working.
-@rpc("any_peer", "call_local", "reliable")
-func say_something():
-	print("A peer was connected")
+# Called when this player is hosting and a client connects to it.
+func _on_peer_connected(id: int) -> void:
+	player_ids[id] = null
+	start_game.rpc()
+
+
+# Called when this player is hosting and a client disconnects from it.
+func _on_peer_disconnected(id: int) -> void:
+	player_ids.erase(id)
+
+
+# Load the main game scene.
+@rpc("authority", "call_local", "reliable")
+func start_game():
+	get_tree().change_scene_to_packed(start_game_scene)
