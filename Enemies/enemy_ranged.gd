3cc049d0e864117231a54c45b065e506570c3faf
Alexander Grams <alexgrams222@gmail.com>
2025-01-29 20:51:21 -0800

    Melee enemies killed by Goth ult becomes allies.

--- Enemies/enemy_ranged.gd
+++ Enemies/enemy_ranged.gd
@@ -7,7 +7,6 @@ extends Enemy
 @export var fire_interval: float = 1.0
 @export var bullet_scene: PackedScene
 
-# NOTE: I don't actually know how much performance we gain by doing this.
 # Used for faster distance calculation
 var squared_max_range: float
 var fire_timer: float = 0.0
@@ -26,14 +25,14 @@ func _process(delta: float) -> void:
 	if fire_timer < fire_interval:
 		fire_timer += delta
 	
-	if player != null:
-		if global_position.distance_squared_to(player.global_position) > squared_max_range:
-			global_position = global_position.move_toward(player.global_position, delta*speed)
+	if target != null:
+		if global_position.distance_squared_to(target.global_position) > squared_max_range:
+			global_position = global_position.move_toward(target.global_position, delta*speed)
 		else:
-			# Shoot at the player
+			# Shoot at the target
 			if is_multiplayer_authority() and fire_timer >= fire_interval:
 				var bullet = bullet_scene.instantiate()
-				var direction = player.global_position - self.global_position
+				var direction = target.global_position - self.global_position
 				var direction_normal = direction.normalized()
 				bullet.set_damage(bullet_damage)
 				bullet.direction = direction_normal
