fe5b24096621785add7e9d9824c496a8d9742582
Alexander Grams <alexgrams222@gmail.com>
2025-02-13 21:09:59 -0800

    Scythe powerup base scene and script.
    Thinking about changing the naming scheme for powerups. Would be a pain to change all the file names though and could cause issues. Probably have to commit to one scheme at this point.

---
+++ Powerups/powerup_scythe.gd
@@ -0,0 +1,39 @@
+extends Powerup
+
+var bullet_scene := "res://Powerups/orbit_bullet.tscn"
+var sprite = preload("res://Coconut.png")
+var bullet
+
+signal picked_up_powerup(sprite)
+
+
+func _ready() -> void:
+	max_level = 15
+	powerup_name = "Orbit"
+	upgrade_curve = load("res://Curves/upgrade_orbit.tres")
+	
+	damage_levels = [20.0, 25.0, 25.0, 25.0, 100.0]
+
+
+func activate_powerup():
+	get_tree().root.get_node("Playground/BulletSpawner").request_spawn_bullet.rpc_id(
+		1,
+		[
+			bullet_scene, 
+			Vector2.ZERO, 
+			Vector2.ZERO, 
+			upgrade_curve.sample(float(current_level) / max_level), 
+			[multiplayer.get_unique_id()]
+		]
+	)
+	
+	picked_up_powerup.emit(sprite)
+
+
+func deactivate_powerup():
+	pass
+
+
+func level_up():
+	current_level += 1
+	powerup_level_up.emit(current_level, upgrade_curve.sample(float(current_level) / max_level))
