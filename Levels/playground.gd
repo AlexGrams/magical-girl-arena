6e22baf414fa40eeb67bbb39fc5c4b22100f7939
Alexander Grams <alexgrams222@gmail.com>
2025-02-15 20:20:15 -0800

    Added Playgruond controller script.
    Will be used to spawn the corrupted enemy after some time. Decided to make this functionality separate from the GameState. Maybe should move more functionality from GameState to the Playground script.

---
+++ Levels/playground.gd
@@ -0,0 +1,31 @@
+class_name Playground
+extends Node2D
+## Script for controlling events that occur during gameplay.
+
+## Time in seconds from the start of the game, expressed as a ratio between the elapsed game 
+## time and the total game time, at which the corrupted magical girl enemy spawns. 0.0 is at the 
+## start of the game, and 1.0 is at the end of the game timer.
+@export var corrupted_enemy_spawn_time_fraction: float = 0.0
+
+var _has_corrupted_enemy_spawned := false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Only process on the server.
+func _process(delta: float) -> void:
+	if not is_multiplayer_authority():
+		return
+	
+	if not _has_corrupted_enemy_spawned:
+		if GameState.get_game_progress_as_fraction() >= corrupted_enemy_spawn_time_fraction:
+			_spawn_corrupted_enemy()
+
+
+# Spawn the corrupted magical girl enemy.
+func _spawn_corrupted_enemy() -> void:
+	_has_corrupted_enemy_spawned = true
+	print("Spawn")
