3092022dd66dd650a1c8c8fc1f25e713115453ad
Alexander Grams <alexgrams222@gmail.com>
2025-02-23 20:47:30 -0800

    Removed old code and objects related to spawning with Curves.

--- Levels/LevelPlaceables/enemy_spawner.gd
+++ Levels/LevelPlaceables/enemy_spawner.gd
@@ -1,21 +1,10 @@
 class_name EnemySpawner
 extends Node2D
 
-## Enemy that is spawned when we want to make a melee enemy.
-@export var enemy_melee_scene: PackedScene
-## Enemy that is spawned when we want to make a ranged enemy.
-@export var enemy_ranged_scene: PackedScene
 ## True when this spawner is able to make Enemies automatically.
 @export var enabled: bool = true
-## True if this spawner makes enemies periodically.
-@export var automatic_spawning := true
 ## The rectangular shape for the area in which enemies will spawn in 
 @export var spawn_area: CollisionShape2D = null
-
-# Time in seconds until the next melee enemy spawn.
-var spawn_timer_melee: float = 0.0
-# Time in seconds until the next ranged enemy spawn.
-var spawn_timer_ranged: float = 0.0
 
 # The bounds for spawning in global coordinates.
 var _spawn_x_min: float = 0
@@ -25,43 +14,11 @@ var _spawn_y_max: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if enemy_melee_scene != null:
-		load(enemy_melee_scene.resource_path)
-	if enemy_ranged_scene != null:
-		load(enemy_ranged_scene.resource_path)
-	
 	var spawn_rect := spawn_area.get_shape().get_rect()
 	_spawn_x_min = global_position.x + spawn_rect.position.x * global_scale.x
 	_spawn_x_max = global_position.x + (spawn_rect.position.x + spawn_rect.size.x) * global_scale.x
 	_spawn_y_min = global_position.y + spawn_rect.position.y * global_scale.y
 	_spawn_y_max = global_position.y + (spawn_rect.position.y + spawn_rect.size.y) * global_scale.y
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	# TODO: Remove once new spawning is implemented.
-	return
-	
-	# Only the server controls spawning
-	if (not multiplayer.is_server()
-		or multiplayer.multiplayer_peer.get_connection_status() != MultiplayerPeer.CONNECTION_CONNECTED):
-		return
-	
-	if automatic_spawning and enabled:
-		spawn_timer_melee -= delta
-		spawn_timer_ranged -= delta
-		
-		if spawn_timer_melee <= 0.0 and enemy_melee_scene != null:
-			spawn(enemy_melee_scene)
-			spawn_timer_melee = GameState.get_melee_spawn_interval()
-		if spawn_timer_ranged <= 0.0 and enemy_ranged_scene != null:
-			spawn(enemy_ranged_scene)
-			spawn_timer_ranged = GameState.get_ranged_spawn_interval()
-
-
-func set_enemy_type(_new_enemy: PackedScene) -> void:
-	push_error("DEPRECATED: May want to change how setting spawner types works.")
-	#enemy_scene_ = new_enemy
 
 
 # Enable or disable this spawner
