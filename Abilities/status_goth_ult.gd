ca040a6beaec88f9ecd54b21a0da6118a46bc3c1
Alexander Grams <alexgrams222@gmail.com>
2025-03-20 22:51:55 -0700

    Created Goth Ult Status scene and script.

---
+++ Abilities/status_goth_ult.gd
@@ -0,0 +1,14 @@
+class_name StatusGothUlt
+extends Node2D
+## Status affecting enemies hit by Goth's ultimate. If the Enemy dies while this status is
+## active, then they are converted to an ally temporarily before despawning.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
