b0ff84926256086be3b9129f1aa0808fb5781319
Alexander Grams <alexgrams222@gmail.com>
2025-03-22 19:42:21 -0700

    Tarot card Powerup max level shoots with more density.

--- Powerups/revolving_powerup.gd
+++ Powerups/revolving_powerup.gd
@@ -1,6 +1,9 @@
 extends Powerup
 
+## Time in seconds between shots.
 @export var shoot_interval: float  = 0.25
+## Time in seconds between shots when the powerup is at max level.
+@export var max_level_shoot_interval: float = 0.06
 @export var bullet_scene := "res://Powerups/revolving_bullet.tscn"
 
 # TODO: Might not be used anymore
@@ -10,6 +13,11 @@ var shoot_timer: float = 0
 var direction := Vector2.RIGHT
 var bullet_damage: float
 
+## Current angle in degrees between one bullet and the next. 
+var _rotation: float = 30.0
+## Angle in degrees between one bullet and next when this powerup is at max level.
+var _max_level_rotation: float = 10.0
+
 signal picked_up_powerup(sprite)
 
 
@@ -17,6 +25,9 @@ func _ready() -> void:
 	# TODO: Make these properties read from the PowerupData instead of setting them here.
 	powerup_name = "Tarot Cards"
 	bullet_damage = upgrade_curve.sample(float(current_level) / max_level)
+	if current_level >= max_level:
+		_rotation = _max_level_rotation
+		shoot_interval = max_level_shoot_interval
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -34,7 +45,7 @@ func _process(delta: float) -> void:
 				]
 			)
 			
-			direction = direction.rotated(deg_to_rad(30)).normalized()
+			direction = direction.rotated(deg_to_rad(_rotation)).normalized()
 			
 			shoot_timer = 0
 
@@ -56,4 +67,10 @@ func deactivate_powerup():
 func level_up():
 	current_level += 1
 	bullet_damage = upgrade_curve.sample(float(current_level) / max_level)
+	
+	# Shoot way faster at 5th level
+	if current_level >= max_level:
+		_rotation = _max_level_rotation
+		shoot_interval = max_level_shoot_interval
+	
 	powerup_level_up.emit(current_level, bullet_damage)
