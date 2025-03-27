58cf59b0d65ecfc7a3d88bfb874b95e037ab40d5
Alexander Grams <alexgrams222@gmail.com>
2025-03-26 18:23:36 -0700

    LootBoxes are destroyable.

--- Levels/LevelPlaceables/loot_box.gd
+++ Levels/LevelPlaceables/loot_box.gd
@@ -4,12 +4,26 @@ extends Node2D
 ## has a chance of spawning items such as gold or health for the player.
 ## Enemies cannot interact with a LootBox.
 
+@export var max_health: float = 100.0
+
+var _health: float = 0.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_health = max_health
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_area_2d_entered(area: Area2D) -> void:
+	if not is_multiplayer_authority():
+		return
+	
+	if area is BulletHitbox:
+		_health -= area.damage
+		if _health <= 0.0:
+			_destroy()
+
+
+## Break this object and create a pickup. Only call on server.
+func _destroy() -> void:
+	queue_free()
