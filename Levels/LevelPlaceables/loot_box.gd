8852df7339f4d0b32491f433625a0326b16ee2e7
Alexander Grams <alexgrams222@gmail.com>
2025-03-26 18:17:43 -0700

    Base LootBox scene and script.
    Destroyable object that drops health or gold.

---
+++ Levels/LevelPlaceables/loot_box.gd
@@ -0,0 +1,15 @@
+class_name LootBox
+extends Node2D
+## A neutral destructable object in the world. When the player breaks it, 
+## has a chance of spawning items such as gold or health for the player.
+## Enemies cannot interact with a LootBox.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
