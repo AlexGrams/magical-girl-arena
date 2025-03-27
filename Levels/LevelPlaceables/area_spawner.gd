1e0c5ab5963f68e161f1b49597ec190327ed5d99
Alexander Grams <alexgrams222@gmail.com>
2025-03-26 20:14:41 -0700

    Moved some functionality of EnemySpawner to parent class AreaSpawner

---
+++ Levels/LevelPlaceables/area_spawner.gd
@@ -0,0 +1,50 @@
+class_name AreaSpawner
+extends Node2D
+## Spawns objects at a random position within an area.
+
+
+## True when this spawner is able to make objects automatically.
+@export var enabled: bool = true
+## The rectangular shape for the area in which enemies will spawn in 
+@export var spawn_area: CollisionShape2D = null
+
+# The bounds for spawning in global coordinates.
+var _spawn_x_min: float = 0
+var _spawn_x_max: float = 0
+var _spawn_y_min: float = 0
+var _spawn_y_max: float = 0
+
+
+func _ready() -> void:
+	var spawn_rect := spawn_area.get_shape().get_rect()
+	_spawn_x_min = global_position.x + spawn_rect.position.x * global_scale.x
+	_spawn_x_max = global_position.x + (spawn_rect.position.x + spawn_rect.size.x) * global_scale.x
+	_spawn_y_min = global_position.y + spawn_rect.position.y * global_scale.y
+	_spawn_y_max = global_position.y + (spawn_rect.position.y + spawn_rect.size.y) * global_scale.y
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+# Enable or disable this spawner
+func set_enabled(new_enabled: bool) -> void:
+	enabled = new_enabled
+
+
+# Toggle this spawner
+func toggle_enabled() -> void:
+	enabled = !enabled
+
+
+# Make an object of this spawner's type within its designated spawn area. Spawning is
+# done periodically by default, but this function can be called to spawn manually.
+func spawn(scene_to_spawn: PackedScene) -> void:
+	var obj = scene_to_spawn.instantiate()
+	var spawn_pos = Vector2(
+		randf_range(_spawn_x_min, _spawn_x_max), 
+		randf_range(_spawn_y_min, _spawn_y_max)
+	)
+	obj.global_position = spawn_pos
+	get_node("..").add_child(obj, true)
--- Levels/LevelPlaceables/enemy_spawner.gd
+++ Levels/LevelPlaceables/enemy_spawner.gd
@@ -1,39 +1,11 @@
 class_name EnemySpawner
-extends Node2D
+extends AreaSpawner
 
-## True when this spawner is able to make Enemies automatically.
-@export var enabled: bool = true
-## The rectangular shape for the area in which enemies will spawn in 
-@export var spawn_area: CollisionShape2D = null
-
-# The bounds for spawning in global coordinates.
-var _spawn_x_min: float = 0
-var _spawn_x_max: float = 0
-var _spawn_y_min: float = 0
-var _spawn_y_max: float = 0
 
 # All spawn events that are running on this spawner.
 # 0: EnemySpawnEventData - information for this spawn event.
 # 1: float - time until the next activation.
 var _active_spawn_events: Array[Array] = []
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var spawn_rect := spawn_area.get_shape().get_rect()
-	_spawn_x_min = global_position.x + spawn_rect.position.x * global_scale.x
-	_spawn_x_max = global_position.x + (spawn_rect.position.x + spawn_rect.size.x) * global_scale.x
-	_spawn_y_min = global_position.y + spawn_rect.position.y * global_scale.y
-	_spawn_y_max = global_position.y + (spawn_rect.position.y + spawn_rect.size.y) * global_scale.y
-
-
-# Enable or disable this spawner
-func set_enabled(new_enabled: bool) -> void:
-	enabled = new_enabled
-
-
-# Toggle this spawner
-func toggle_enabled() -> void:
-	enabled = !enabled
 
 
 func _process(delta: float) -> void:
@@ -59,21 +31,8 @@ func _process(delta: float) -> void:
 			
 		spawn_event[1] -= delta
 		i += 1
-		
 
 
-# Make an enemy of this spawner's type within its designated spawn area. Spawning is
-# done periodically by default, but this function can be called to spawn manually.
-func spawn(scene_to_spawn: PackedScene) -> void:
-	var enemy = scene_to_spawn.instantiate()
-	var spawn_pos = Vector2(
-		randf_range(_spawn_x_min, _spawn_x_max), 
-		randf_range(_spawn_y_min, _spawn_y_max)
-	)
-	enemy.global_position = spawn_pos
-	get_node("..").add_child(enemy, true)
-
-
-# Asynchronous function to spawn enemies repeatedly from this spawner.
+# Function to spawn enemies repeatedly from this spawner.
 func spawn_repeating(spawn_event: EnemySpawnEventData, start_time: float = 0.0) -> void:
 	_active_spawn_events.append([spawn_event, start_time])
