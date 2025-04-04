02667424172fab1a8c1d0c89d7598d273e9b6ddf
Alexander Grams <alexgrams222@gmail.com>
2025-04-03 19:54:06 -0700

    Fixed health pickup not restoring health on client characters.

--- Pickups/health_orb.gd
+++ Pickups/health_orb.gd
@@ -8,7 +8,7 @@ extends EXPOrb
 
 ## Destroys this object and adds health. Called when it touches any player.
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if uncollected and is_multiplayer_authority() and area.get_collision_layer_value(4):
+	if uncollected and multiplayer.get_unique_id() == player.get_multiplayer_authority()and area.get_collision_layer_value(4):
 		uncollected = false
 		if player is PlayerCharacterBody2D:
 			player.take_damage.rpc(-abs(HEALTH_TO_ADD))
