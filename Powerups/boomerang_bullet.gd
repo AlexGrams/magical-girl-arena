35aa91e9522d446eedfbfeeea21b5abb152403e7
Victoria W <56736494+Tori-W@users.noreply.github.com>
2025-04-04 15:15:21 -0700

    Changed boomerang to farthest enemy
    Giving it a try

--- Powerups/boomerang_bullet.gd
+++ Powerups/boomerang_bullet.gd
@@ -7,7 +7,11 @@ const touching_distance_threshold: float = 30
 # This owner is the client's replicated version of the character that owns this bullet.
 var boomerang_owner: Node2D = null
 
-var closest_enemy: Node
+var farthest_enemy: Node
+
+# Target enemy must be within this range
+var max_range: float = 1000
+
 var is_returning := true
 
 ## When owned by an Enemy, the location that the bullet to moving towards away from its owner. 
@@ -37,17 +41,18 @@ func _process(delta: float) -> void:
 				enemies = get_tree().get_nodes_in_group("player")
 				
 			if !enemies.is_empty():
-				closest_enemy = enemies[0]
-				var closest_distance = global_position.distance_squared_to(enemies[0].global_position)
+				farthest_enemy = null
+				var farthest_distance = 0
 				for enemy in enemies:
 					var distance = global_position.distance_squared_to(enemy.global_position)
-					if distance < closest_distance:
-						closest_enemy = enemy
-						closest_distance = distance
+					print(distance)
+					if distance > farthest_distance and distance <= (max_range * max_range):
+						farthest_enemy = enemy
+						farthest_distance = distance
 			
 			# An Enemy Boomerang moves towards a static location instead of chasing the Player.
 			if not _is_owned_by_player:
-				_target_location = closest_enemy.global_position
+				_target_location = farthest_enemy.global_position
 			
 			# Stop returning and start moving out
 			is_returning = false
@@ -55,9 +60,9 @@ func _process(delta: float) -> void:
 		if _is_owned_by_player:
 			# If the closest enemy still exists, move towards them
 			# Otherwise return to player
-			if closest_enemy != null:
-				global_position += (closest_enemy.global_position - global_position).normalized() * speed * delta
-				if (closest_enemy.global_position - global_position).length() <= touching_distance_threshold:
+			if farthest_enemy != null:
+				global_position += (farthest_enemy.global_position - global_position).normalized() * speed * delta
+				if (farthest_enemy.global_position - global_position).length() <= touching_distance_threshold:
 					is_returning = true
 			else:
 				is_returning = true
