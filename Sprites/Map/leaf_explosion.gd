f994b24bb947e2167eb5207a3d856c4475ae78a9
Alexander Grams <alexgrams222@gmail.com>
2025-04-04 21:28:46 -0700

    Removed leaf explosion testing prints.

--- Sprites/Map/leaf_explosion.gd
+++ Sprites/Map/leaf_explosion.gd
@@ -2,9 +2,7 @@ extends GPUParticles2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Explosion set")
 	emitting = true
 
 func _on_finished() -> void:
-	print("Deleting explosion")
 	queue_free()
