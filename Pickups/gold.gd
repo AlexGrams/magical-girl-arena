112e7b9bf802379045014774c5e644e84aa5ebab
Victoria W <56736494+Tori-W@users.noreply.github.com>
2025-04-05 13:22:20 -0700

    Added gold pickup SFX

--- Pickups/gold.gd
+++ Pickups/gold.gd
@@ -5,5 +5,6 @@ extends EXPOrb
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if uncollected and is_multiplayer_authority() and area.get_collision_layer_value(4):
 		uncollected = false
+		AudioManager.create_audio_at_location(global_position, SoundEffectSettings.SOUND_EFFECT_TYPE.ON_GOLD_PICKUP)
 		GameState.collect_gold.rpc()
 		destroy.rpc_id(1) 
