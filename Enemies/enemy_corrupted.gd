ab4cd330b07e0cd392fa1359acd07fe6309ef52d
Alexander Grams <alexgrams222@gmail.com>
2025-04-08 23:44:56 -0700

    Corrupted enemies make boss health bar update.
    Appears, updates values, and disappears based on Corrupted Enemy health.

--- Enemies/enemy_corrupted.gd
+++ Enemies/enemy_corrupted.gd
@@ -11,12 +11,16 @@ extends EnemyRanged
 var current_lifetime: float = 0.0
 
 var _signature_powerup_orb: PackedScene = preload("res://Pickups/signature_powerup_orb.tscn")
+var _hud_canvas_layer: HUDCanvasLayer = null
 
 
 func _ready() -> void:
 	super()
 	
 	current_lifetime = corrupted_lifetime
+	
+	_hud_canvas_layer = get_tree().root.get_node("Playground/CanvasLayer")
+	_hud_canvas_layer.show_boss_health_bar(float(health) / max_health)
 	
 	var default_powerup: PowerupData = load(default_powerup_path)
 	if default_powerup != null:
@@ -39,6 +43,40 @@ func shoot() -> void:
 	pass
 
 
+## Asynchronously set up the loot orb dropped by this Corrupted Enemy.
+func _set_up_signature_powerup_orb(signature_powerup_orb: SignaturePowerupOrb) -> void:
+	signature_powerup_orb.teleport.rpc(global_position)
+	signature_powerup_orb.set_powerup.rpc(default_powerup_path)
+
+
+## Only call on the server. Deals damage to this corrupted enemy. Update health bars on all clients.
+@rpc("any_peer", "call_local")
+func _take_damage(damage: float) -> void:
+	super(damage)
+	
+	_hud_canvas_layer.update_boss_health_bar.rpc(float(health) / max_health)
+
+
+## Delete the corrupted enemy. Only call on the server.
+@rpc("any_peer", "call_local")
+func die() -> void:
+	if not is_multiplayer_authority():
+		return
+	
+	super()
+	_hud_canvas_layer.hide_boss_health_bar.rpc()
+
+
+# Despawn the corrupted enemy. Different from dying as it doesn't drop loot.
+@rpc("any_peer", "call_local")
+func _leave() -> void:
+	if not is_multiplayer_authority():
+		return
+	
+	_hud_canvas_layer.hide_boss_health_bar.rpc()
+	queue_free()
+
+
 ## Spawns a Powerup Pickup after being defeated if at least one player can use it. 
 ## Otherwise, gives a lot of experience.
 func _spawn_loot() -> void:
@@ -53,18 +91,3 @@ func _spawn_loot() -> void:
 		, CONNECT_DEFERRED
 	)
 	get_tree().root.get_node("Playground").call_deferred("add_child", signature_powerup_orb, true)
-
-
-## Asynchronously set up the loot orb dropped by this Corrupted Enemy.
-func _set_up_signature_powerup_orb(signature_powerup_orb: SignaturePowerupOrb) -> void:
-	signature_powerup_orb.teleport.rpc(global_position)
-	signature_powerup_orb.set_powerup.rpc(default_powerup_path)
-
-
-# Despawn the corrupted enemy. Different from dying as it doesn't drop loot.
-@rpc("any_peer", "call_local")
-func _leave() -> void:
-	if not is_multiplayer_authority():
-		return
-	
-	queue_free()
