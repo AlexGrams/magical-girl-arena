f17352dfc3e692d985231f70d04bdbe845240fb1
Alexander Grams <alexgrams222@gmail.com>
2025-04-13 20:13:16 -0700

    Pet Powerup moves to an nearby Enemy when it gets within range.

--- Powerups/bullet_pet.gd
+++ Powerups/bullet_pet.gd
@@ -1,12 +1,44 @@
 class_name BulletPet
-extends Bullet
+extends CharacterBody2D
+
+
+## How fast the pet moves in units/second.
+@export var _speed: float = 100.0
+## Area component for checking which Enemies are within range of the pet.
+@export var _attack_area: Area2D = null
+
+## Which Enemy the pet is currently trying to attack.
+var _target: Enemy = null
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	super()
+	pass
+	#super()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _physics_process(delta: float) -> void:
+	if _target:
+		velocity = (_target.global_position - global_position) * _speed
+		move_and_slide()
+	else:
+		_get_highest_health_nearby_enemy()
+
+
+## Sets the Pet's target to the Enemy with the highest health value in the nearby range.
+func _get_highest_health_nearby_enemy() -> void:
+	for area: Area2D in _attack_area.get_overlapping_areas():
+		var highest_health_enemy = null
+		
+		if area.get_parent() is Enemy:
+			var enemy: Enemy = area.get_parent()
+			if not highest_health_enemy or enemy.health > highest_health_enemy.health:
+				highest_health_enemy = enemy
+		if highest_health_enemy:
+			_target = highest_health_enemy
+			print(_target.name)
--- Powerups/powerup_pet.gd
+++ Powerups/powerup_pet.gd
@@ -1,4 +1,5 @@
 extends Powerup
+## Might be unique in that this powerup doesn't create a Bullet.
 
 
 ## UID of the pet scene bullet.
@@ -13,15 +14,18 @@ func _ready():
 
 func activate_powerup():
 	if _is_owned_by_player:
-		get_tree().root.get_node("Playground/BulletSpawner").request_spawn_bullet.rpc_id(
-			1, [pet_scene, 
-				global_position, 
-				Vector2.UP, 
-				_get_damage_from_curve(), 
-				_is_owned_by_player,
-				[$"..".get_path()]
-			]
-		)
+		var pet: BulletPet = load(pet_scene).instantiate()
+		get_tree().root.get_node("Playground").add_child(pet, true)
+		pet.global_position = global_position
+		#get_tree().root.get_node("Playground/BulletSpawner").request_spawn_bullet.rpc_id(
+			#1, [pet_scene, 
+				#global_position, 
+				#Vector2.UP, 
+				#_get_damage_from_curve(), 
+				#_is_owned_by_player,
+				#[$"..".get_path()]
+			#]
+		#)
 	else:
 		# TODO: Support for when owned by an enemy.
 		pass
