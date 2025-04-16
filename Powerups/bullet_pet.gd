2f4990e9e9164928eb9a2c23ba888036bf3b8afb
Alexander Grams <alexgrams222@gmail.com>
2025-04-15 22:24:27 -0700

    Pet attacks repeatedly while touching an Enemy.

--- Powerups/bullet_pet.gd
+++ Powerups/bullet_pet.gd
@@ -4,26 +4,41 @@ extends CharacterBody2D
 
 ## How fast the pet moves in units/second.
 @export var _speed: float = 100.0
+## Time in seconds between attacks.
+@export var _attack_time: float = 1.0
+## Component for damage collisions.
+@export var _bullet_hitbox: BulletHitbox = null
 ## Area component for checking which Enemies are within range of the pet.
 @export var _attack_area: Area2D = null
 
 ## Which Enemy the pet is currently trying to attack.
 var _target: Enemy = null
+## Current time until the next attack.
+var _attack_timer: float = 0.0
+## Default collision layer for the pet's attack.
+var _bullet_collision_layer: int = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-	#super()
+	_bullet_collision_layer = _bullet_hitbox.collision_layer
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	# To attack, flicker the Bullet hitbox collision area for one frame.
+	if _attack_time == _attack_timer:
+		_bullet_hitbox.collision_layer = _bullet_collision_layer
+	
+	_attack_timer -= delta
+	
+	if _attack_timer < 0.0:
+		_bullet_hitbox.collision_layer = 0
+		_attack_timer = _attack_time
 
 
 func _physics_process(delta: float) -> void:
-	if _target:
+	if _target != null:
 		velocity = (_target.global_position - global_position) * _speed
 		move_and_slide()
 	else:
