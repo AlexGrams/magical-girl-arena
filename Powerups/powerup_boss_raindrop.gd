69f05f8bbc253ec6c03ca486d11343c80436e35f
Alexander Grams <alexgrams222@gmail.com>
2025-04-23 21:13:40 -0700

    Raindrop Powerup also spawns randomly around the Boss.
    New boss mechanics are complete.

--- Powerups/powerup_boss_raindrop.gd
+++ Powerups/powerup_boss_raindrop.gd
@@ -3,12 +3,17 @@ extends Powerup
 
 
 @export var _damage: float = 25.0
-## Time in seconds between activations
-@export var _shoot_interval: float = 1.0
+## Time in seconds between activation of bullets directly on the players.
+@export var _targeted_shoot_interval: float = 2.0
+## Time in seconds between activation of bullets spread randomly around the owner.
+@export var _random_shoot_interval: float = 0.2
+## Maximum distance in units away from the owner that random bullets can spawn.
+@export var _random_shoot_radius: float = 1000.0
 ## UID of the scene for the powerup bullets
 @export var _bullet_scene_uid := ""
 
-var _shoot_timer: float = 0.0
+var _targeted_shoot_timer: float = 0.0
+var _random_shoot_timer: float = 0.0
 
 
 func _ready() -> void:
@@ -18,15 +23,21 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if is_on:
-		_shoot_timer -= delta
-		if _shoot_timer <= 0.0:
-			_shoot()
-			_shoot_timer = _shoot_interval
+		_targeted_shoot_timer -= delta
+		_random_shoot_timer -= delta
+		if _targeted_shoot_timer <= 0.0:
+			_shoot_targeted()
+			_targeted_shoot_timer = _targeted_shoot_interval
+		if _random_shoot_timer <= 0.0:
+			_shoot_random()
+			_random_shoot_timer = _random_shoot_interval
+		
 
 
 func activate_powerup():
 	is_on = true
-	_shoot_timer = _shoot_interval
+	_targeted_shoot_timer = _targeted_shoot_interval
+	_random_shoot_timer = _random_shoot_interval
 
 
 # For when adding this powerup to an Enemy when it is usually added to a Player.
@@ -39,8 +50,8 @@ func deactivate_powerup():
 	is_on = false
 
 
-## Spawn in timed raindrop bullets.
-func _shoot() -> void:
+## Spawn in timed activation bullets directly on the players.
+func _shoot_targeted() -> void:
 	# Some raindrops appear directly over all players that aren't downed.
 	for player: PlayerCharacterBody2D in GameState.player_characters.values():
 		if not player.is_down:
@@ -52,3 +63,21 @@ func _shoot() -> void:
 				false,
 				[]
 			])
+
+
+## Spawn in timed activation bullets randomly around the boss.
+func _shoot_random() -> void:
+	var location: Vector2 = (
+		global_position + 
+		Vector2.UP.rotated(randf_range(0.0, 2 * PI)) * 
+		randf_range(0.0, _random_shoot_radius)
+	)
+	
+	get_tree().root.get_node("Playground/BulletSpawner").request_spawn_bullet.rpc_id(
+	1, [_bullet_scene_uid, 
+		location, 
+		Vector2.ZERO, 
+		_damage, 
+		false,
+		[]
+	])
