52476bfc5a36da07fea526989c023c39968cd871
Alexander Grams <alexgrams222@gmail.com>
2025-04-24 18:12:05 -0700

    Boss moves in a 5-pointed star pattern
    Boss updates done.

--- Enemies/enemy_boss_test.gd
+++ Enemies/enemy_boss_test.gd
@@ -1,18 +1,24 @@
 extends EnemyBoss
 
 
+## Squared distance for which the boss is considered to be at one of its movement points.
+const _squared_destination_theshold: float = 100.0
+
 ## Time in seconds between when the boss switches which attack pattern its doing.
 @export var _pattern_interval: float = 5.0
+## The distance of the points that the boss moves between from its spawn position
+@export var _movement_radius: float = 500.0
 ## UIDs of Powerup scenes to add to the boss.
 @export var powerups_to_add: Array[String] = []
-## Lower values means the boss moves around in a larger circle.
-@export var rotation_rate: float = 0.5
 
 var _current_rotation: float = 0.0
 ## Time in seconds until the next pattern switch.
 var _pattern_switch_timer: float = 0.0
 ## Which attack pattern the boss is currently doing.
 var _pattern_index: int = 0
+## The five points of the star that the boss moves between as its regular movement pattern.
+var _movement_points: Array[Vector2] = []
+var _current_movement_point: int = 0
 
 
 func _ready() -> void:
@@ -49,11 +55,28 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	if target != null:
-		_current_rotation += rotation_rate * delta
-		velocity = Vector2.LEFT.rotated(_current_rotation) * speed
+		if global_position.distance_squared_to(_movement_points[_current_movement_point]) <= _squared_destination_theshold:
+			# Boss has completed one movement, so find the next point to move towards.
+			_current_movement_point = (_current_movement_point + 2) % 5
+		
+		velocity = (_movement_points[_current_movement_point] - global_position).normalized() * speed
 		move_and_slide()
 	else:
 		if is_multiplayer_authority():
 			_find_new_target()
 		else:
 			move_and_slide()
+
+
+## Move this enemy to a location.
+@rpc("authority", "call_local")
+func teleport(pos: Vector2) -> void:
+	global_position = pos
+	
+	# Calculate the five points of the star that the boss moves between after it is teleported to its
+	# starting position.
+	if len(_movement_points) == 0:
+		var direction := Vector2.UP * _movement_radius
+		for i in range(5):
+			_movement_points.append(direction + global_position)
+			direction = direction.rotated(deg_to_rad(72.0))
