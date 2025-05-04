24f0a4538c8779fc8ed745535d22f1e1e342681a
Victoria W <56736494+Tori-W@users.noreply.github.com>
2025-05-04 13:41:49 -0700

    Added stat icons

--- UI/stat_level_indicator.gd
+++ UI/stat_level_indicator.gd
@@ -3,7 +3,10 @@ extends VBoxContainer
 
 
 @export var _label: Label = null
+@export var icon: Texture = null
 
+func _ready() -> void:
+	$StatIcon.texture = icon
 
 ## Sets the stat level displayed on this indicator
 func set_stat_value(new_value: int) -> void:
