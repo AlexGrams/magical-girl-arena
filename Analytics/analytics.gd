78c61b431a21da30425cf114e622d56f38da3563
Alexander Grams <alexgrams222@gmail.com>
2025-05-14 20:31:23 -0700

    Analytic event is sent to GameAnalytics server.
    Events appear on the "Live Events" panel on the web client. Problem was there was a missing DLL in the addon folder. May want to try deleting the remaining files later, as I just put the entire repository into our project under the "game_analytics" folder.
    Next: Figure out how to organize events on the dashboard to figure out what's going on with our game.

--- Analytics/analytics.gd
+++ Analytics/analytics.gd
@@ -8,6 +8,7 @@ var game_analytics
 func _ready():
 	# ... other code from your project ...
 	if(Engine.has_singleton("GameAnalytics")):
+		push_warning("Start it up")
 		game_analytics = Engine.get_singleton("GameAnalytics")
 		game_analytics.setEnabledInfoLog(true)
 		game_analytics.setEnabledVerboseLog(true)
@@ -18,4 +19,20 @@ func _ready():
 		game_analytics.configureAvailableResourceCurrencies(["exp", "gold"])
 		game_analytics.configureAvailableResourceItemTypes(["ItemA", "ItemB"])
 
-		game_analytics.init(["6fb0a1013dc107c344db069cb7a0f27d"], ["88d77ae183bc7cbf99b0fb3d1eed16eed8698842"])
+		game_analytics.init(
+			"6fb0a1013dc107c344db069cb7a0f27d", 		# Game key
+			"88d77ae183bc7cbf99b0fb3d1eed16eed8698842"	# Secret key
+		)
+		
+		# TODO: Testing sending events
+		game_analytics.addDesignEvent({
+			"eventId": "BossFights:FireLord:KillTimeUsed",
+  			"value": 234
+		})
+		
+		game_analytics.addDesignEvent({"eventId": "BBossFights:FireLord:KillTimeUsed"})
+		
+		game_analytics.addErrorEvent({
+			"severity": "Debug",
+			"message": "Testing sending events from the game."
+		})
