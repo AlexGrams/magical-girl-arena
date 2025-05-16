ffc4083f23589bd57a0293c588f17290afd9474f
Alexander Grams <alexgrams222@gmail.com>
2025-05-15 21:21:14 -0700

    Reuse anonymous login info if it already exists.
    That way we aren't making tons of new users on the free PlayFab account.

--- Analytics/analytics.gd
+++ Analytics/analytics.gd
@@ -5,7 +5,17 @@ extends PlayFabEvent
 
 func _ready():
 	# First, we need to log in to send any events to the PlayFab servers
-	PlayFabManager.client.login_anonymous()
+	if PlayFabManager.client_config.login_type == PlayFabClientConfig.LoginType.LOGIN_CUSTOM_ID:
+		# User doesn't have an anonymous account yet, so make a new one.
+		var combined_info_request_params = GetPlayerCombinedInfoRequestParams.new()
+		combined_info_request_params.show_all()
+		var player_profile_view_constraints = PlayerProfileViewConstraints.new()
+		combined_info_request_params.ProfileConstraints = player_profile_view_constraints
+		
+		PlayFabManager.client.login_with_custom_id(PlayFabManager.client_config.login_id, false, combined_info_request_params)
+	else:
+		push_warning("Creating new anonymous user")
+		PlayFabManager.client.login_anonymous()
 	print("Attempting login...")
 	
 	PlayFabManager.client.logged_in.connect(func(_login_result: LoginResult):
