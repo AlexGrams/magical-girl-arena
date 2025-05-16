20acf6f47284ddbea6888cbe572b76d780e79b32
Alexander Grams <alexgrams222@gmail.com>
2025-05-15 19:14:52 -0700

    Added Microsoft Azure PlayFab addon for analytics.
    Going to see if PlayFab works better than GameAnalytics, which doesn't make it easy to record that data that we want.

--- Analytics/analytics.gd
+++ Analytics/analytics.gd
@@ -1,38 +1,7 @@
 extends Node
-## Manages game analytics through GameAnalytics: https://www.gameanalytics.com/
-
-
-var game_analytics
+## Manages game analytics through Microsoft Azure PlayFab: https://playfab.com/
+## Docs: https://learn.microsoft.com/en-us/gaming/playfab/get-started/  
 
 
 func _ready():
-	# ... other code from your project ...
-	if(Engine.has_singleton("GameAnalytics")):
-		push_warning("Start it up")
-		game_analytics = Engine.get_singleton("GameAnalytics")
-		game_analytics.setEnabledInfoLog(true)
-		game_analytics.setEnabledVerboseLog(true)
-
-		game_analytics.configureBuild("0.1.0")
-
-		game_analytics.configureAvailableCustomDimensions01(["CustomA", "CustomB"])
-		game_analytics.configureAvailableResourceCurrencies(["exp", "gold"])
-		game_analytics.configureAvailableResourceItemTypes(["ItemA", "ItemB"])
-
-		game_analytics.init(
-			"6fb0a1013dc107c344db069cb7a0f27d", 		# Game key
-			"88d77ae183bc7cbf99b0fb3d1eed16eed8698842"	# Secret key
-		)
-		
-		# TODO: Testing sending events
-		game_analytics.addDesignEvent({
-			"eventId": "BossFights:FireLord:KillTimeUsed",
-  			"value": 234
-		})
-		
-		game_analytics.addDesignEvent({"eventId": "BBossFights:FireLord:KillTimeUsed"})
-		
-		game_analytics.addErrorEvent({
-			"severity": "Debug",
-			"message": "Testing sending events from the game."
-		})
+	pass
