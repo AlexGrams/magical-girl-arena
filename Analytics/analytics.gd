7a75a7030d200957ed4c94dec72fbca11e437058
Alexander Grams <alexgrams222@gmail.com>
2025-05-15 21:09:05 -0700

    Trying to get PlayFab telemetry events working.
    Need to fix bug where the PlayFab http object or whatever isn't being initialized before attempting to send the telemetry event.

--- Analytics/analytics.gd
+++ Analytics/analytics.gd
@@ -1,7 +1,23 @@
-extends Node
+extends PlayFabEvent
 ## Manages game analytics through Microsoft Azure PlayFab: https://playfab.com/
 ## Docs: https://learn.microsoft.com/en-us/gaming/playfab/get-started/  
 
 
 func _ready():
-	pass
+	# First, we need to log in to send any events to the PlayFab servers
+	PlayFabManager.client.login_anonymous()
+	print("Attempting login...")
+	
+	PlayFabManager.client.logged_in.connect(func(_login_result: LoginResult):
+		# Next, try to send a telemetry event.
+		#var event_name := "test_telemetry_event"
+		#var payload := {
+			#"key_string": "value",
+			#"key_float": 1.5,
+			#"key_int": 3
+		#}
+		#
+		#write_title_player_telemetry_event(event_name, payload)
+		print("Telemetry event sent!")
+	)
+	
