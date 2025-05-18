fa452657afd8b0f0dfd85b150bbcd4a4572b7a50
Alexander Grams <alexgrams222@gmail.com>
2025-05-18 16:27:36 -0700

    Analytics sends game version number.
    Game version needs to be manually set each patch in the Project Settings.

--- Analytics/analytics.gd
+++ Analytics/analytics.gd
@@ -20,9 +20,9 @@ func _ready():
 	# First, we need to log in to send any events to the PlayFab servers
 	if PlayFabManager.client_config.login_type == PlayFabClientConfig.LoginType.LOGIN_CUSTOM_ID:
 		# User doesn't have an anonymous account yet, so make a new one.
-		var combined_info_request_params = GetPlayerCombinedInfoRequestParams.new()
+		var combined_info_request_params := GetPlayerCombinedInfoRequestParams.new()
 		combined_info_request_params.show_all()
-		var player_profile_view_constraints = PlayerProfileViewConstraints.new()
+		var player_profile_view_constraints := PlayerProfileViewConstraints.new()
 		combined_info_request_params.ProfileConstraints = player_profile_view_constraints
 		
 		PlayFabManager.client.login_with_custom_id(PlayFabManager.client_config.login_id, false, combined_info_request_params)
@@ -52,6 +52,7 @@ func _send_telemerty_event_callback(_data: Dictionary) -> void:
 func _setup_telemetry_payload() -> void:
 	_telemetry_payload = {
 		"match_id": 0,
+		"version": ProjectSettings.get_setting("application/config/version"),
 		"character": "",
 		"level_up_times": [],
 		"upgrades_chosen": [],
