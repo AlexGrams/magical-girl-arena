430d248ef5001e803a908141296e6c424babb5c7
Victoria W <56736494+Tori-W@users.noreply.github.com>
2025-05-19 17:26:05 -0700

    Added DoveArrow sprite

--- Powerups/TarotCardSprite.gd
+++ Powerups/TarotCardSprite.gd
@@ -1,9 +1,10 @@
 extends Sprite2D
 
+@export var degree_offset:float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	rotation = $"..".direction.angle() + deg_to_rad(90)
+	rotation = $"..".direction.angle() + deg_to_rad(degree_offset)
 	
 func _process(_delta) -> void:
 	#rotate(45 * delta)
