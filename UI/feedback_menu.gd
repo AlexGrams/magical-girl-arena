69c7df12e0193199859a251b84bbaf73195dfb65
Alexander Grams <alexgrams222@gmail.com>
2025-05-24 22:16:28 -0700

    Feedback menu base script and scene.

---
+++ UI/feedback_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+## Used for inputting written feedback from users and sending it to our PlayFab database using Telemetry events.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
