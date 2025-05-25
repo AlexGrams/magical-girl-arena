bc0e5bcd2d0557aa0522f91f3fba54310ba6c3b9
Alexander Grams <alexgrams222@gmail.com>
2025-05-24 23:03:16 -0700

    Feedback menu show and hide from the Main Menu

--- UI/feedback_menu.gd
+++ UI/feedback_menu.gd
@@ -1,6 +1,13 @@
 extends Control
 ## Used for inputting written feedback from users and sending it to our PlayFab database using Telemetry events.
 
+## We are limited with how big of a message we can send to the PlayFab servers using telemetetry events (10kB).
+## I believe that this limit is large if the message is just text, but this is to ensure that it is never reached.
+## More: https://learn.microsoft.com/en-us/gaming/playfab/data-analytics/legacy/insights/best-practices 
+const MAX_CHARACTERS = 8000
+
+@export var _text_box: TextEdit = null
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,3 +17,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_submit_down() -> void:
+	# TODO: Submit text through analytics
+	hide()
