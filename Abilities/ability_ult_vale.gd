4e37241e397dba54b7a745c79324634d7735484e
Alexander Grams <alexgrams222@gmail.com>
2025-05-30 20:44:43 -0700

    Base assets for new Vale ultimate

---
+++ Abilities/ability_ult_vale.gd
@@ -0,0 +1,10 @@
+extends Ability
+## Click areas on the screen to bring down large bombs that deal massive damage. 
+
+
+func _process(delta: float) -> void:
+	super(delta)
+
+
+func activate() -> void:
+	super()
