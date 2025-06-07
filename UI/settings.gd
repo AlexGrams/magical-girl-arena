f931e9b82dfab71a29dee483ef2354eb84433a09
Victoria W <56736494+Tori-W@users.noreply.github.com>
2025-06-07 04:51:29 -0700

    Added number input box to volume slider

--- UI/settings.gd
+++ UI/settings.gd
@@ -5,6 +5,8 @@ const max_volume_slider_value: float = 1.25
 
 ## Slider for setting the game volume.
 @export var _volume_slider: Slider = null
+## Spinbox for setting the game volume
+@export var _spinbox: SpinBox = null
 
 var _screen_modes := [DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN, DisplayServer.WINDOW_MODE_WINDOWED]
 
@@ -17,6 +19,15 @@ func _ready() -> void:
 func _on_screen_mode_item_selected(index: int) -> void:
 	DisplayServer.window_set_mode(_screen_modes[index])
 
-
+# Volume slider range is 0 - 1
 func _on_volume_slider_drag_ended(value_changed: bool) -> void:
-	AudioManager.set_volume_multiplier(_volume_slider.value * max_volume_slider_value)
+	var new_slider_value = _volume_slider.value
+	_spinbox.value = new_slider_value * 100
+	# Changing Spinbox value will emit the value_changed signal, so we don't need to set Audio again
+	#AudioManager.set_volume_multiplier(new_volume_value * max_volume_slider_value)
+
+# Spinbox range is 0 - 100
+func _on_spin_box_value_changed(spinbox_value: float) -> void:
+	AudioManager.set_volume_multiplier((spinbox_value / 100) * max_volume_slider_value)
+	if (spinbox_value / 100) != _volume_slider.value:
+		_volume_slider.value = spinbox_value / 100
