e41d7b22a836133fcb9ef0e1cce1df5e50c2a752
Victoria W <56736494+Tori-W@users.noreply.github.com>
2025-06-08 17:43:42 -0700

    Separated volume slider visual into music and SFX
    - TODO: Actual music volume functionality. SFX currently works.

--- UI/settings.gd
+++ UI/settings.gd
@@ -5,10 +5,14 @@ const max_volume_slider_value: float = 1.25
 
 ## OptionButton for display mode.
 @export var _screen_mode_option: OptionButton = null
-## Slider for setting the game volume.
-@export var _volume_slider: Slider = null
-## Spinbox for setting the game volume
-@export var _spinbox: SpinBox = null
+## Slider for setting the main menu and battle music volume.
+@export var _music_volume_slider: Slider = null
+## Spinbox for setting the main menu and battle music volume volume
+@export var _music_spinbox: SpinBox = null
+## Slider for setting the sfx volume.
+@export var _sfx_volume_slider: Slider = null
+## Spinbox for setting the sfx volume
+@export var _sfx_spinbox: SpinBox = null
 
 var _screen_modes := [DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN, DisplayServer.WINDOW_MODE_WINDOWED]
 
@@ -17,8 +21,8 @@ var _screen_modes := [DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN, DisplaySer
 func _ready() -> void:
 	var settings: ConfigFile = SettingsManager.get_settings()
 	_screen_mode_option.selected = _screen_modes.find(settings.get_value("display", "display_mode"))
-	_volume_slider.value = settings.get_value("sound", "volume") / max_volume_slider_value
-	_spinbox.value = _volume_slider.value * 100
+	_sfx_volume_slider.value = settings.get_value("sound", "volume") / max_volume_slider_value
+	_sfx_spinbox.value = _sfx_volume_slider.value * 100
 
 
 func _on_screen_mode_item_selected(index: int) -> void:
@@ -27,17 +31,17 @@ func _on_screen_mode_item_selected(index: int) -> void:
 
 # Volume slider range is 0 - 1
 func _on_volume_slider_drag_ended(_value_changed: bool) -> void:
-	var new_slider_value = _volume_slider.value
-	_spinbox.value = new_slider_value * 100
+	var new_slider_value = _sfx_volume_slider.value
+	_sfx_spinbox.value = new_slider_value * 100
 
 
 # Spinbox range is 0 - 100
 func _on_spin_box_value_changed(spinbox_value: float) -> void:
 	SettingsManager.apply_volume((spinbox_value / 100) * max_volume_slider_value)
-	if (spinbox_value / 100) != _volume_slider.value:
-		_volume_slider.value = spinbox_value / 100
+	if (spinbox_value / 100) != _sfx_volume_slider.value:
+		_sfx_volume_slider.value = spinbox_value / 100
 
 
 ## Writes values set on this screen to disk.
 func _save_settings_changes() -> void:
-	SaveManager.save_settings(_screen_modes[_screen_mode_option.selected], _volume_slider.value * max_volume_slider_value)
+	SaveManager.save_settings(_screen_modes[_screen_mode_option.selected], _sfx_volume_slider.value * max_volume_slider_value)
