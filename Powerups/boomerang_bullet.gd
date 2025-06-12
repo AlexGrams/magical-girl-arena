2c9e855cbd09c171d3aac9fd8d65184b4785a317
Alexander Grams <alexgrams222@gmail.com>
2025-06-12 16:51:01 -0700

    Boomerang bounces between players in multiplayer.
    No longer really works for single player and multiplayer.

--- Powerups/boomerang_bullet.gd
+++ Powerups/boomerang_bullet.gd
@@ -1,4 +1,9 @@
 extends Bullet
+## Boomerang bounces between all player characters.
+##
+## NOTE: Powerup does not currently work correctly if someone (such as the host) spawns in with the 
+## boomerang before all the other characters have spawned in.
+
 
 ## How close the Boomerang needs to get to its destination before switching directions.
 const touching_distance_threshold: float = 30.0
@@ -6,15 +11,16 @@ const touching_distance_threshold: float = 30.0
 ## Target enemy must be within this range
 @export var max_range: float = 750
 
+@onready var _squared_touching_distance_threshold: float = touching_distance_threshold ** 2
 ## The bullet object is replicated on all clients.
 ## This owner is the client's replicated version of the character that owns this bullet.
-var boomerang_owner: Node2D = null
-var farthest_enemy: Node
-var is_returning := true
-
-@onready var _squared_touching_distance_threshold: float = touching_distance_threshold ** 2
-## When owned by an Enemy, the location that the bullet to moving towards away from its owner. 
-var _target_location := Vector2.ZERO
+var _boomerang_owner: Node2D = null
+## Which node the boomerang is currently moving towards.
+var _target: Node2D = null
+## List of nodes that the boomerang can bounce between.
+var _all_targets: Array[Node2D] = []
+## index of _all_targets that the boomerang is currently moving towards.
+var _target_index: int = 0
 
 
 func _ready() -> void:
@@ -22,52 +28,23 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	if is_returning:
-		if boomerang_owner == null or boomerang_owner.is_queued_for_deletion():
-			return
-		
-		# Move towards player
-		global_position += (boomerang_owner.global_position - global_position).normalized() * speed * delta
-		
-		# If close enough to player, send out again
-		if (boomerang_owner.global_position - global_position).length() <= 30:
-			# Get next nearest character to attack
-			var enemies: Array[Node] = [] 
-			if _is_owned_by_player:
-				enemies = get_tree().get_nodes_in_group("enemy")
-			else:
-				enemies = get_tree().get_nodes_in_group("player")
-				
-			if !enemies.is_empty():
-				farthest_enemy = null
-				var farthest_distance = 0
-				for enemy in enemies:
-					var distance = global_position.distance_squared_to(enemy.global_position)
-					if distance > farthest_distance and distance <= (max_range * max_range):
-						farthest_enemy = enemy
-						farthest_distance = distance
-			
-			# An Enemy Boomerang moves towards a static location instead of chasing the Player.
-			if not _is_owned_by_player:
-				_target_location = farthest_enemy.global_position
-			
-			# Stop returning and start moving out
-			is_returning = false
-	else:
-		if _is_owned_by_player:
-			# If the closest enemy still exists, move towards them
-			# Otherwise return to player
-			if farthest_enemy != null:
-				global_position += (farthest_enemy.global_position - global_position).normalized() * speed * delta
-				if (farthest_enemy.global_position - global_position).length() <= touching_distance_threshold:
-					is_returning = true
-			else:
-				is_returning = true
-		else:
-			# Enemy Boomerang behavior
-			global_position += (_target_location - global_position).normalized() * speed * delta
-			if (_target_location - global_position).length() <= touching_distance_threshold:
-				is_returning = true
+	if not _is_owned_by_player:
+		push_error("Boomerange not configured for use by Enemies.")
+		return
+	
+	if (
+			_boomerang_owner == null
+			or _boomerang_owner.is_queued_for_deletion()
+			or len(_all_targets) <= 0
+	):
+		return
+	
+	global_position += (_target.global_position - global_position).normalized() * speed * delta
+	
+	# Choose the next target if we are close enough to our current target.
+	if global_position.distance_squared_to(_target.global_position) <= _squared_touching_distance_threshold:
+		_target_index = (_target_index + 1) % len(_all_targets)
+		_target = _all_targets[_target_index]
 
 
 # Set up other properties for this bullet
@@ -77,13 +54,25 @@ func setup_bullet(is_owned_by_player: bool, data: Array) -> void:
 		or typeof(data[0]) != TYPE_NODE_PATH	# Owning character 
 	):
 		return
+	print(multiplayer.get_unique_id())
 	
-	boomerang_owner = get_tree().root.get_node(data[0])
+	_boomerang_owner = get_tree().root.get_node(data[0])
+	global_position = _boomerang_owner.global_position
+	
+	# The authority over this bullet sets the order by which the boomerang bounces between targets.
+	if is_multiplayer_authority():
+		_all_targets.assign(GameState.player_characters.values())
+		_target_index = 0
+		_target = _all_targets[_target_index]
+	else:
+		# If we are the client, we need to request from the server the order in which to visit targets.
+		# This is done using two RPCs, which will complete this frame.
+		request_all_targets.rpc_id(1)
 	
 	_is_owned_by_player = is_owned_by_player
 	if is_owned_by_player:
 		# When the player levels up this powerup, notify all clients about the level up.
-		var boomerang_powerup := boomerang_owner.get_node_or_null("BoomerangPowerup")
+		var boomerang_powerup := _boomerang_owner.get_node_or_null("BoomerangPowerup")
 		# The Powerup child is not replicated, so only the client which owns this character has it.
 		if boomerang_powerup != null:
 			boomerang_powerup.powerup_level_up.connect(func(new_level: int, new_damage: float):
@@ -91,7 +80,7 @@ func setup_bullet(is_owned_by_player: bool, data: Array) -> void:
 			)
 	
 		# When the owner goes down, destroy this bullet
-		boomerang_owner.died.connect(func():
+		_boomerang_owner.died.connect(func():
 			queue_free()
 		)
 	else:
@@ -99,13 +88,33 @@ func setup_bullet(is_owned_by_player: bool, data: Array) -> void:
 		
 		# Destroy bullet when owning Enemy dies
 		if is_multiplayer_authority():
-			boomerang_owner.died.connect(func(_enemy: Enemy):
+			_boomerang_owner.died.connect(func(_enemy: Enemy):
 				queue_free()
 			)
 
 
 func set_damage(damage:float):
 	$Area2D.damage = damage
+
+
+## Call from a client only to request the order in which targets are visited from the server.
+@rpc("any_peer", "call_remote")
+func request_all_targets() -> void:
+	var node_paths: Array[NodePath]
+	for node: Node2D in _all_targets:
+		node_paths.append(node.get_path())
+	set_all_targets.rpc_id(multiplayer.get_remote_sender_id(), node_paths, _target_index, global_position)
+
+
+## Gives a client the order in which this boomerang will bounce between targets.
+@rpc("authority", "call_local")
+func set_all_targets(targets: Array[NodePath], current_index: int, current_position: Vector2) -> void:
+	_all_targets.clear()
+	for path: NodePath in targets:
+		_all_targets.append(get_tree().root.get_node(path))
+	_target_index = current_index
+	_target = _all_targets[_target_index]
+	global_position = current_position
 
 
 # This bullet's owner has leveled up this bullet's corresponding powerup
