0f965d0bedf75e100bb2782ff50f3f9a248729f4
Victoria W <56736494+Tori-W@users.noreply.github.com>
2025-06-22 01:32:48 -0700

    Added PingPong visuals and updated description

---
+++ Powerups/PingPong_Sprite.gd
@@ -0,0 +1,11 @@
+extends Sprite2D
+
+@export var PingPongBullet:Node2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = PingPongBullet._target.global_position - global_position
+	rotation = direction.angle() + deg_to_rad(90)
