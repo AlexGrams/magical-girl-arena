db4d16e9fffb625e3ccd897d8a1e02e1f4530ad5
Victoria W <56736494+Tori-W@users.noreply.github.com>
2025-06-29 13:54:21 -0700

    Added new Goth model + animations

--- Player/PlayerSprite.gd
+++ Player/PlayerSprite.gd
@@ -53,9 +53,8 @@ func _ready() -> void:
 # Called by death signal
 func play_death_animation() -> void:
 	is_dead = true
-	if character_data.name != "Goth": # GOTH IS NOT DONE
-		$Blinking_AnimationPlayer.stop()
-		$Live2D_AnimationPlayer.play(character_data.name + "_Death")
+	$Blinking_AnimationPlayer.stop()
+	$Live2D_AnimationPlayer.play(character_data.name + "_Death")
 
 func play_revive_animation() -> void:
 	is_dead = false
