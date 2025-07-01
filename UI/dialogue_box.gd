61b4edbc68d1cc4e3711d6691ca5875cf3092311
Alexander Grams <alexgrams222@gmail.com>
2025-06-30 19:59:30 -0700

    Dialogue box HUD element and dialogue loading logic.
    The DialogueBox loads all DialogueData in a certain file. Extra functionality to get it to work in release builds. Stores data after it is loaded for access later on.

---
+++ UI/dialogue_box.gd
@@ -0,0 +1,32 @@
+class_name DialogueBox
+extends Control
+## Displays lines of dialogue. Determines when dialogue is played and randomly decides which 
+## dialogue to run.
+
+
+## Parent folder for dialogue resources.
+const DIALOGUE_DATA_FOLDER_PATH: String = "res://Dialogue/DialogueData/"
+
+## Map containing DialogueData for all dialogue sequences in the game.
+## Key: Constants.DialoguePlayTrigger - When the dialogue can be played.
+## Value: Array[DialogueData] - List of dialogues that can be played at that trigger.
+var _dialogue: Dictionary = {}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for dialogue_data_path: String in DirAccess.open(DIALOGUE_DATA_FOLDER_PATH).get_files():
+		# Exporting adds ".remap" to the end of .tres files.
+		if '.tres.remap' in dialogue_data_path:
+			dialogue_data_path = dialogue_data_path.trim_suffix('.remap')
+		
+		var dialogue_data: DialogueData = ResourceLoader.load(DIALOGUE_DATA_FOLDER_PATH + dialogue_data_path)
+		if not _dialogue.has(dialogue_data.play_trigger):
+			_dialogue[dialogue_data.play_trigger] = []
+		_dialogue[dialogue_data.play_trigger].append(dialogue_data)
+	print(_dialogue)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
