1377452ea8e20d639db81cdb9a411b3326232fb4
Alexander Grams <alexgrams222@gmail.com>
2025-06-30 21:23:04 -0700

    Dialogue line container UI scene and script.
    Scales properly with text size, but background isn't set up to tile the correct way.

---
+++ UI/dialogue_line_container.gd
@@ -0,0 +1,18 @@
+class_name DialogueLineContainer
+extends Control
+## Displays a line of dialogue with a speaker portrait.
+
+## Image of who is speaking.
+@export var _portrait: TextureRect = null
+## Dialogue text.
+@export var _text: Label = null
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
