a79dae275d3b0654bbd69a0d173b262d14dbd889
Alexander Grams <alexgrams222@gmail.com>
2025-07-02 21:52:24 -0700

    Added Bounce powerup.
    Bullet bounces to nearest enemy when it hits.

---
+++ Powerups/bullet_bounce.gd
@@ -0,0 +1,72 @@
+extends Bullet
+
+## How close this bullet needs to get to its destination before switching targets.
+const TOUCHING_DISTANCE_THRESHOLD: float = 30.0
+
+
+## How many targets this bullet hits before it is destroyed.
+@export var _max_bounces: int = 3
+@onready var _squared_touching_distance_threshold: float = TOUCHING_DISTANCE_THRESHOLD ** 2
+## Current remaining number of enemies this bullet can hit before it is destroyed.
+@onready var _bounces: int = _max_bounces 
+## Enemy that this bullet is moving towards.
+var _target: Node = null
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	#if _target.is_queued_for_deletion():
+		#_target = null
+	
+	if _target != null:
+		global_position += (_target.global_position - global_position).normalized() * speed * delta
+		if _target.global_position.distance_squared_to(global_position) <= _squared_touching_distance_threshold:
+			_find_new_target()
+	
+	# Bounce if we hit our target or if they're not valid for some reason.
+	if _target == null:
+		_bounces -= 1
+		if _bounces <= 0:
+			if is_multiplayer_authority():
+				queue_free()
+		else:
+			_find_new_target()
+
+
+## Set this bullet's target to the nearest enemy that isn't the current target.
+func _find_new_target() -> void:
+	var enemies: Array[Node] = get_tree().get_nodes_in_group("enemy")
+		
+	if !enemies.is_empty():
+		var nearest_enemy: Node2D = enemies[0]
+		var nearest_distance = global_position.distance_squared_to(enemies[0].global_position)
+		
+		for enemy in enemies:
+			if enemy == _target:
+				continue
+			
+			var distance = global_position.distance_squared_to(enemy.global_position)
+			if distance < nearest_distance:
+				nearest_enemy = enemy
+				nearest_distance = distance
+		
+		_target = nearest_enemy
+
+
+## Set up other properties for this bullet
+func setup_bullet(is_owned_by_player: bool, data: Array) -> void:
+	if (
+		data.size() != 1
+		or typeof(data[0]) != TYPE_NODE_PATH	# Path to first target 
+	):
+		push_error("Malformed data array")
+		return
+	
+	_target = get_tree().root.get_node(data[0])
+	_is_owned_by_player = is_owned_by_player
+	if not is_owned_by_player:
+		push_warning("Not implemented for enemies.")
+		return
---
+++ Powerups/powerup_bounce.gd
@@ -0,0 +1,57 @@
+extends Powerup
+
+
+## Time in seconds between firing.
+@export var _fire_interval = 1.0
+## Path to the Bullet-derived bullet scene.
+@export var _bullet_scene := ""
+## Path to the PowerupData resource file for this Powerup.
+@export var _powerup_data_file_path: String = ""
+
+var _fire_timer: float = 0.0
+
+
+func _ready() -> void:
+	powerup_name = load(_powerup_data_file_path).name
+
+
+func _process(delta: float) -> void:
+	if not is_on:
+		return
+	
+	_fire_timer += delta
+	if _fire_timer > _fire_interval:
+		get_tree().root.get_node("Playground/BulletSpawner").request_spawn_bullet.rpc_id(
+			1, 
+			[
+				_bullet_scene, 
+				global_position, 
+				Vector2.UP, 
+				_get_damage_from_curve(), 
+				_is_owned_by_player,
+				multiplayer.get_unique_id(),
+				_powerup_index,
+				[_find_nearest_target().get_path()]
+			]
+		)
+		
+		# TODO: Play sound effect
+		# AudioManager.create_audio_at_location(global_position, SoundEffectSettings.SOUND_EFFECT_TYPE.CUPID_ARROW)
+		_fire_timer = 0.0
+
+
+func activate_powerup():
+	is_on = true
+
+
+func deactivate_powerup():
+	is_on = false
+	_fire_timer = 0.0
+
+
+func level_up():
+	current_level += 1
+	
+	## TODO: Extra bonus here.
+	if current_level >= 3:
+		pass
