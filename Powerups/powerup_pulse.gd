5f2a46e7b2e77584d8e713588bb95eccbb25b2f8
Victoria W <56736494+Tori-W@users.noreply.github.com>
2025-07-13 13:22:04 -0700

    Added Pulse level 3
    Pulse sends out twice

--- Powerups/powerup_pulse.gd
+++ Powerups/powerup_pulse.gd
@@ -7,9 +7,12 @@ extends Powerup
 @export var _bullet_scene := ""
 ## Path to the PowerupData resource file for this Powerup.
 @export var _powerup_data_file_path: String = ""
+## How long between double pulses (level 3+)
+@export var _double_pulse_interval: float = 0.1
 
 var _fire_timer: float = 0.0
 var _owner: Node = null
+var _double_pulse_started: bool = false
 
 
 func _ready() -> void:
@@ -23,6 +26,7 @@ func _process(delta: float) -> void:
 	
 	_fire_timer += delta
 	if _fire_timer > _fire_interval:
+		print("HELLO")
 		get_tree().root.get_node("Playground/BulletSpawner").request_spawn_bullet.rpc_id(
 			1, 
 			[
@@ -36,6 +40,25 @@ func _process(delta: float) -> void:
 				[_owner.get_path()]
 			]
 		)
+		if current_level >= 3 and !_double_pulse_started:
+			# Bool needed, otherwise spawns multiple pulses due to the way
+			# _process works with timers
+			_double_pulse_started = true
+			await get_tree().create_timer(_double_pulse_interval).timeout
+			get_tree().root.get_node("Playground/BulletSpawner").request_spawn_bullet.rpc_id(
+			1, 
+			[
+				_bullet_scene, 
+				global_position, 
+				Vector2.UP, 
+				_get_damage_from_curve(), 
+				_is_owned_by_player,
+				multiplayer.get_unique_id(),
+				_powerup_index,
+				[_owner.get_path()]
+			]
+			)
+			_double_pulse_started = false
 		
 		# TODO: Play sound effect
 		# AudioManager.create_audio_at_location(global_position, SoundEffectSettings.SOUND_EFFECT_TYPE.CUPID_ARROW)
@@ -53,7 +76,3 @@ func deactivate_powerup():
 
 func level_up():
 	current_level += 1
-	
-	## TODO: Extra bonus here.
-	if current_level >= 3:
-		pass
