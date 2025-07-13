ccd4ac3a5c5387928529562d2ac51c24ce6df8cc
Victoria W <56736494+Tori-W@users.noreply.github.com>
2025-07-13 14:21:30 -0700

    New Bullet_Turret_Bullet.gd script for level 3 upgrades

---
+++ Powerups/bullet_turret_bullet.gd
@@ -0,0 +1,23 @@
+class_name Bullet_Turret_Bullet
+extends Bullet
+## Bullet that is shot by turrets.
+## Designed to allow for additional level 3 upgrade effects
+
+## Set up other properties for this bullet
+func setup_bullet(is_owned_by_player: bool, data: Array) -> void:
+	if (
+		data.size() != 1
+		or typeof(data[0]) != TYPE_BOOL 	# Level 3 upgrade or not
+	):
+		push_error("Malformed data array")
+		return
+
+	# Level 3 upgrade - increase in size
+	if data[0]:
+		scale = scale * 2
+	
+	# Make the bullet hurt players
+	if not is_owned_by_player:
+		_is_owned_by_player = false
+		_health = max_health
+		_modify_collider_to_harm_players()
