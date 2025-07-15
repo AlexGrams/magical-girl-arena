6041019247eca3310b76026e0dae68248566756f
Alexander Grams <alexgrams222@gmail.com>
2025-07-14 18:17:05 -0700

    Slight simplification of DamageIndicator by replacing the animation with tweens.
    Not really noticeable improvement in performance. The remaining performance hit possibly comes from creating a new tween each time.

--- UI/damage_indicator.gd
+++ UI/damage_indicator.gd
@@ -11,12 +11,25 @@ var scale_multiplier:float = 2
 var _base_scale:float = 1
 ## The original scale of the base image.
 var _starting_texture_scale: Vector2 = Vector2.ONE
+var _starting_big_sparkle_color: Color
+var _starting_small_sparkle_color: Color
+var _ending_big_sparkle_color: Color
+var _ending_small_sparkle_color: Color
+var _tween: Tween = null
 
 
 func _ready() -> void:
 	_starting_texture_scale = $TextureRect.scale
+	_starting_big_sparkle_color = $TextureRect.self_modulate
+	_starting_small_sparkle_color = $TextureRect/TextureRect2.self_modulate
+	_ending_big_sparkle_color = _starting_big_sparkle_color
+	_ending_small_sparkle_color = _starting_small_sparkle_color
+	_ending_big_sparkle_color.a = 0.0
+	_ending_small_sparkle_color.a = 0.0
+	
 	# Randomize the position once for each indicator.
 	$TextureRect.position = Vector2(randf_range(-50, 50), randf_range(-50, 50))
+	
 	hide()
 
 
@@ -47,17 +60,22 @@ func animate(pos: Vector2, damage_value: float) -> void:
 	else:
 		_base_scale = 0.25
 	
+	# Set initial values
 	time_passed = 0.0
-	
-	# Set initial scale
 	$TextureRect.scale = _starting_texture_scale * _base_scale
+	$TextureRect.self_modulate = _starting_big_sparkle_color
+	$TextureRect/TextureRect2.self_modulate = _starting_small_sparkle_color
 	
-	var tween = create_tween()
-	tween.set_ease(Tween.EASE_OUT)
-	tween.set_trans(Tween.TRANS_ELASTIC)
-	tween.tween_property($TextureRect, "scale", $TextureRect.scale * scale_multiplier, 0.3)
-	tween.tween_property($TextureRect, "scale", Vector2.ZERO, 0.7)
-	$AnimationPlayer.play("float")
+	if _tween != null:
+		_tween.kill()
+	
+	_tween = create_tween()
+	_tween.set_ease(Tween.EASE_OUT)
+	_tween.parallel().tween_property($TextureRect, "self_modulate", _ending_big_sparkle_color, 1.0)
+	_tween.parallel().tween_property($TextureRect/TextureRect2, "self_modulate", _ending_small_sparkle_color, 1.0)
+	_tween.set_trans(Tween.TRANS_ELASTIC)
+	_tween.parallel().tween_property($TextureRect, "scale", $TextureRect.scale * scale_multiplier, 0.3)
+	_tween.tween_property($TextureRect, "scale", Vector2.ZERO, 0.7)
 	
 	show()
 	set_process(true)
