d003a8bc4b7668393014757fd300a6264b6d44a0
Alexander Grams <alexgrams222@gmail.com>
2025-08-03 21:19:46 -0700

    Andromeda script clean up variables.

--- Enemies/enemy_boss_andromeda.gd
+++ Enemies/enemy_boss_andromeda.gd
@@ -6,20 +6,12 @@ extends EnemyBoss
 ## When the boss is at this fraction of its max health, switch to phase 2.
 @export var _phase_two_health_fraction: float = 0.15
 ## Paths to Powerup scenes for the boss's phase one attacks.
-@export var _powerup_paths_phase_one: Array[String] = []
-## Paths to Powerup scenes for the boss's phase two attacks.
-@export var _powerup_paths_phase_two: Array[String] = []
+@export var _powerup_paths: Array[String] = []
 
-## Phase one powerups
-var _powerups_one: Array[Powerup]
-## Phase two powerups 
-var _powerups_two: Array[Powerup]
 ## Time in seconds until the next pattern switch.
 var _pattern_switch_timer: float = 0.0
-## Phase one powerup attack index.
-var _powerup_index_one: int = 0
-## Which attack pattern the boss is currently doing.
-var _current_attack: Powerup = null
+## Powerup attack index.
+var _powerup_index: int = 0
 var _phase_two_active: bool = false
 
 
@@ -34,18 +26,11 @@ func _ready() -> void:
 		set_process(false)
 		return
 	
-	# Instantiating phase one powerups
-	for powerup_path: String in _powerup_paths_phase_one:
+	# Instantiating powerups
+	for powerup_path: String in _powerup_paths:
 		var powerup: PackedScene = load(powerup_path)
 		if powerup != null:
 			_add_powerup(powerup, false)
-			_powerups_one.append(_powerups[-1])
-	# Instantiating phase two powerups
-	for powerup_path: String in _powerup_paths_phase_two:
-		var powerup: PackedScene = load(powerup_path)
-		if powerup != null:
-			_add_powerup(powerup, false)
-			_powerups_two.append(_powerups[-1])
 	
 	for powerup: Powerup in _powerups:
 		powerup.deactivate_powerup()
@@ -58,48 +43,48 @@ func _process(delta: float) -> void:
 		# Do the next attack in the pattern
 		if float(health) / max_health > _phase_two_health_fraction:
 			# Phase one attack pattern
-			match _powerup_index_one:
+			match _powerup_index:
 				0:
 					# Regular attacks while immune
 					set_is_invulnerable(true)
-					_powerups_one[0].activate_powerup()
+					_powerups[0].activate_powerup()
 				1:
 					# Create terrain
 					set_is_invulnerable(false)
-					_powerups_one[0].deactivate_powerup()
-					_powerups_one[1].activate_powerup()
+					_powerups[0].deactivate_powerup()
+					_powerups[1].activate_powerup()
 				2:
 					# Scream, destroy terrain
-					_powerups_one[2].activate_powerup()
+					_powerups[2].activate_powerup()
 				3:
 					# Chains + regular attacks
-					_powerups_one[3].activate_powerup()
-					_powerups_one[0].activate_powerup()
+					_powerups[3].activate_powerup()
+					_powerups[0].activate_powerup()
 		else:
 			# Phase two attack pattern. Regular attacks are always active.
 			if not _phase_two_active:
 				_phase_two_active = true
-				for powerup: Powerup in _powerups_one:
+				for powerup: Powerup in _powerups:
 					powerup.deactivate_powerup()
-				_powerups_one[0].activate_powerup()
-				_powerup_index_one = 0
+				_powerups[0].activate_powerup()
+				_powerup_index = 0
 			
-			match _powerup_index_one:
+			match _powerup_index:
 				0:
 					# Regular attacks while immune
 					set_is_invulnerable(true)
 				1:
 					# Create terrain
 					set_is_invulnerable(false)
-					_powerups_one[1].activate_powerup()
+					_powerups[1].activate_powerup()
 				2:
 					# Scream, destroy terrain
-					_powerups_one[2].activate_powerup()
+					_powerups[2].activate_powerup()
 				3:
 					# Chains + regular attacks
-					_powerups_one[3].activate_powerup()
+					_powerups[3].activate_powerup()
 		
-		_powerup_index_one = (_powerup_index_one + 1) % 4
+		_powerup_index = (_powerup_index + 1) % 4
 		_pattern_switch_timer = _pattern_interval
 
 
