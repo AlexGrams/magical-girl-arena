6ff62f3c237c3e9c7eeb4a1e247f6d652f1034d2
Alexander Grams <alexgrams222@gmail.com>
2025-08-17 19:20:32 -0700

    Created MapData resource for storing map information.
    Will be used to load a map and access its associated information.

---
+++ Levels/map_data.gd
@@ -0,0 +1,9 @@
+class_name MapData
+extends Resource
+## Stores important properties for each map.
+
+
+## Name of this map as displayed to the player.
+@export var name: String = ""
+## Path to the scene to load for this map.
+@export var scene_path: String = ""
