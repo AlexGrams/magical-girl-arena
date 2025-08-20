fc462c4ab5e230a122f6825d800a7e40034f77cd
Victoria W <56736494+Tori-W@users.noreply.github.com>
2025-08-19 19:39:58 -0700

    Finished visuals for Andromeda terrain attack
    - Warning is now rumbling cracks in the ground
    - Terrain animates in and out
    - Dust clouds appear when terrain animates in

--- Powerups/bullet_boss_terrain.gd
+++ Powerups/bullet_boss_terrain.gd
@@ -1,19 +1,37 @@
 class_name BulletBossTerrain
 extends Bullet
 
+## Y values for each dust cloud spawned. Manually put in because I'm lazy.
+const Y_VALUES:Array[float] = [-150, -75, 0, 75, 150]
 
 ## Time that the warning for the attacks appears before the damage volume becomes active.
 @export var _tell_time: float = 2.0
 ## Time that this bullet remains active to damage players.
 @export var _damage_time: float = 0.5
 @export var _static_body: StaticBody2D = null
+## Sprite for rumbling warning
+@export var _rumbling_sprite: Sprite2D
+## Sprite for the actual terrain that damages and collides with players
+@export var _terrain_sprite: Sprite2D
+## GPUParticles2D for Dust cloud VFX
+@export var _dust_cloud_particles: Resource
 
 var _tell_timer: float = 0.0
 var _damage_timer: float = 0.0
 var _static_body_collision_layer: int = 0
+# Whether or not the terrain has already appeared for the first time.
+# Prevents dust cloud VFX from happening over and over
+var _terrain_has_appeared: bool = false
 
 
 func _ready() -> void:
+	# Show rumbling cracks
+	_rumbling_sprite.show()
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_QUINT)
+	tween.tween_property(_rumbling_sprite, "scale", Vector2(_rumbling_sprite.scale.y, _rumbling_sprite.scale.y), 0.1)
+	
 	add_to_group("bullet_boss_terrain")
 
 
@@ -26,6 +44,8 @@ func _process(delta: float) -> void:
 			_damage_timer = _damage_time
 	elif _damage_timer > 0.0:
 		# Stage 2: Bullet can damage players.
+		if !_terrain_has_appeared:
+			show_terrain()
 		_damage_timer -= delta
 		if _damage_timer <= 0.0:
 			collider.collision_layer = 0
@@ -51,7 +71,27 @@ func setup_bullet(is_owned_by_player: bool, data: Array) -> void:
 		_static_body_collision_layer = _static_body.collision_layer
 		_static_body.collision_layer = 0
 
+func show_terrain():
+	_terrain_has_appeared = true
+	_rumbling_sprite.hide()
+	_terrain_sprite.show()
+	
+	# Terrain pops up
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_QUINT)
+	tween.tween_property(_terrain_sprite, "scale", Vector2(_terrain_sprite.scale.x, _terrain_sprite.scale.x), 0.1)
+	
+	# Dust clouds appear
+	for y_value in Y_VALUES:
+		var dust_cloud:GPUParticles2D = _dust_cloud_particles.instantiate()
+		dust_cloud.position = Vector2(0, y_value)
+		add_child(dust_cloud)
 
 ## Remove this object. Only call on server.
 func destroy() -> void:
-	queue_free()
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_QUINT)
+	tween.tween_property(_terrain_sprite, "scale", Vector2(_terrain_sprite.scale.x, 0), 0.1)
+	tween.tween_callback(queue_free)
