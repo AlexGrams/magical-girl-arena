0c2df64892f621344bf794fa81dbaeb94a024462
Alexander Grams <alexgrams222@gmail.com>
2025-08-25 16:28:59 -0700

    Tumbleweed LootBoxes move across the Desert map.

--- Levels/LevelPlaceables/loot_box_tumbleweed.gd
+++ Levels/LevelPlaceables/loot_box_tumbleweed.gd
@@ -6,7 +6,21 @@ extends LootBox
 @export var _character_body_2d: CharacterBody2D = null
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-	_character_body_2d.velocity = Vector2.RIGHT * _speed
 	_character_body_2d.move_and_slide()
+	
+	# Hardcoded values to determine when the tumbleweed goes off the map.
+	if is_multiplayer_authority() and global_position.x < -2000 or global_position.x > 4000:
+		queue_free()
+
+
+## Move LootBox to a location. Call using RPC for replication.
+@rpc("authority", "call_local")
+func teleport(pos: Vector2) -> void:
+	super(pos)
+	
+	# Sort of a hack to move in the opposite direction across the arena.
+	if global_position.x > 0:
+		_character_body_2d.velocity = Vector2.LEFT * _speed
+	else:
+		_character_body_2d.velocity = Vector2.RIGHT * _speed
