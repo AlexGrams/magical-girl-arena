ade99cfbb196ae83df110c76e71507a0f52610b2
Alexander Grams <alexgrams222@gmail.com>
2025-08-26 21:02:40 -0700

    Ball can be kicked around by players and bounces off of walls.

--- Powerups/bullet_ball.gd
+++ Powerups/bullet_ball.gd
@@ -1,5 +1,12 @@
+class_name BulletBall
 extends Bullet
 
+
+## Impulse applied to the ball when it touches a player.
+@export var _kick_impulse: float = 10000.0
+## Torque impule applied to the ball when it is kicked.
+@export var _kick_torque: float = 900.0
+@export var _rigidbody: RigidBody2D = null
 
 var _owning_player: PlayerCharacterBody2D = null
 var _crit_chance: float = 0.0
@@ -8,7 +15,8 @@ var _crit_multiplier: float = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if not is_multiplayer_authority():
+		set_process(false)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -53,3 +61,30 @@ func setup_bullet(is_owned_by_player: bool, data: Array) -> void:
 @rpc("any_peer", "call_local")
 func _level_up(_new_level: int, new_damage: float):
 	collider.damage = new_damage
+
+
+## Set how visible this bullet is using the local client's bullet opacity setting.
+func _update_bullet_opacity() -> void:
+	pass
+
+
+func _on_player_kick_area_2d_entered(area: Area2D) -> void:
+	var other: Node2D = area.get_parent()
+	if other is PlayerCharacterBody2D:
+		var direction: Vector2 = (global_position - other.global_position).normalized()
+		_rigidbody.apply_force(direction * _kick_impulse)
+		
+		if direction.x > 0:
+			_rigidbody.apply_torque_impulse(_kick_torque)
+		else:
+			_rigidbody.apply_torque_impulse(-_kick_torque)
+		print("KICK")
+		
+			
+		#for i: int in range(get_slide_collision_count()):
+			#var collision: KinematicCollision2D = get_slide_collision(i)
+			#var collider: Object = collision.get_collider()
+			#var colliding_object: Node = collider.get_parent()
+			#if collider is RigidBody2D and collider is BulletBall:
+				#print("Kick")
+				#collider.apply_force(collision.get_normal() * -300.0)
