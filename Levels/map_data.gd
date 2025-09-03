edb2c5b2716d1193599e38502afa8a5d86cf59c9
Alexander Grams <alexgrams222@gmail.com>
2025-09-03 15:15:59 -0700

    Desert map shows new preview image on Lobby screen.

--- Levels/map_data.gd
+++ Levels/map_data.gd
@@ -7,6 +7,8 @@ extends Resource
 @export var name: String = ""
 ## Path to the scene to load for this map.
 @export var scene_path: String = ""
+## Image that shows on the Lobby for this map.
+@export var preview_image_texture: Texture = null
 ## Name of the GameState variable that has to be true in order to unlock this map, if any.
 @export var required_map_save_variable_name: String = ""
 ## The dialogue play condition specfic to this map.
