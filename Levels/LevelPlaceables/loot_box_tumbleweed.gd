90724f3612bafa90f1234f5cc08313902b372376
Alexander Grams <alexgrams222@gmail.com>
2025-09-14 15:04:02 -0700

    Tumbleweed LootBoxes are hidden if they move over an abyss on Desert map.

--- Levels/LevelPlaceables/loot_box_tumbleweed.gd
+++ Levels/LevelPlaceables/loot_box_tumbleweed.gd
@@ -5,6 +5,10 @@ extends LootBox
 @export var _speed: float = 200.0 
 @export var _character_body_2d: CharacterBody2D = null
 @export var _animation_player:AnimationPlayer
+@export var _area: Area2D = null
+
+## True when the Tumbleweed is above an abyss.
+var _is_invulnerable: bool = false
 
 
 func _physics_process(_delta: float) -> void:
@@ -27,3 +31,29 @@ func teleport(pos: Vector2) -> void:
 	else:
 		_character_body_2d.velocity = Vector2.RIGHT * _speed
 		_animation_player.play("rolling_right")
+
+
+func _teleport(pos: Vector2) -> void:
+	global_position = pos
+
+
+func _on_area_2d_entered(area: Area2D) -> void:
+	# Don't take damage if we're on top of an abyss right now.
+	if area.get_parent() is DesertMapPiece:
+		hide()
+		_is_invulnerable = true
+	
+	if _is_invulnerable:
+		return
+	
+	super(area)
+
+
+func _on_area_2d_exited(area: Area2D) -> void:
+	if area.get_parent() is DesertMapPiece:
+		# Don't remove invulnerability if still colliding with another map piece.
+		for other: Area2D in _area.get_overlapping_areas():
+			if other.get_parent() is DesertMapPiece:
+				return
+		_is_invulnerable = false
+		show()
