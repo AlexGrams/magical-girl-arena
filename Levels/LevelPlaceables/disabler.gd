8e243374d0ec688a11405f3a8098e9ba8ce50afe
Alexander Grams <57524026+AlexGrams@users.noreply.github.com>
2025-09-30 15:01:07 -0700

    Simple Disabler logic to disable and reenable powerups.
    Next: Make a new way to tracking powerup disabling or enabling based on counting sources of disabling/enabled. Has to interact well with getting new powerups and being downed in the disabler zone.

--- Levels/LevelPlaceables/disabler.gd
+++ Levels/LevelPlaceables/disabler.gd
@@ -3,20 +3,36 @@ extends Node2D
 ## A Node that disables all powerups except those of a certain type when a player is nearby.
 
 
-
+## The type of powerups that aren't affected by this Disabler.
+var _allow_type: Powerup.Type = Powerup.Type.NULL
 
 
 func _ready() -> void:
-	pass # Replace with function body.
+	_allow_type = randi_range(1, Powerup.Type.size() - 1)
 
 
 func _process(delta: float) -> void:
 	pass
 
 
+## Disable all powerups that aren't of a certain type on the local player.
 func _on_disable_area_2d_area_entered(area: Area2D) -> void:
-	print(area.get_parent().name)
+	var other = area.get_parent()
+	
+	if other != null and other == GameState.get_local_player():
+		var player: PlayerCharacterBody2D = other
+		print(_allow_type)
+		for powerup: Powerup in player.powerups:
+			if not powerup.has_type(_allow_type):
+				powerup.deactivate_powerup()
 
 
 func _on_disable_area_2d_area_exited(area: Area2D) -> void:
-	print(area.get_parent().name)
+	var other = area.get_parent()
+	
+	if other != null and other == GameState.get_local_player():
+		var player: PlayerCharacterBody2D = other
+		print(_allow_type)
+		for powerup: Powerup in player.powerups:
+			if not powerup.has_type(_allow_type):
+				powerup.activate_powerup()
