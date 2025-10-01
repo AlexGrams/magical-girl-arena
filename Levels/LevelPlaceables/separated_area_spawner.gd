b5e77d2538da0a786b828356814b89fe6f439aa7
Alexander Grams <alexgrams222@gmail.com>
2025-09-30 20:02:40 -0700

    Generalized LootBoxSpawner to work for any Node2D.
    Next: Will be used to spawn Disablers on the Alien map.

--- Levels/LevelPlaceables/separated_area_spawner.gd
+++ Levels/LevelPlaceables/separated_area_spawner.gd
@@ -1,36 +1,39 @@
-class_name LootBoxSpawner
+class_name SeparatedAreaSpawner
 extends AreaSpawner
+## Spawns a scene periodically where each object is at least a certain distance from 
+## objects on a set of collision layers.
 
 
 ## The most times that we will attempt to generate a random position that isn't near a player.
 const MAX_POSITION_CHECKS: int = 30
-## At least how far the LootBox will be spawned from any player. Prevents the LootBox from spawning on top of someone.
-const MIN_DISTANCE_FROM_NEAREST_PLAYER: float = 400.0
-## The radius of the physics query used to determine if the chosen spawn location collides with obstacles.
-## Should be at least as big as the size of a LootBox.
-const COLLISION_CHECK_RADIUS: float = 100.0
 
-## Time in seconds between spawning LootBoxes
+## Time in seconds between spawning objects.
 @export var spawn_interval: float = 1.0
-@export var loot_box_scene: PackedScene = null
+@export var spawn_object_scene: PackedScene = null
+
+## Minimum spawn distance from the player
+@export var _min_distance_from_nearest_player: float = 400.0
+## The radius of the physics query used to determine if the chosen spawn location collides with obstacles.
+## Should be at least as big as the size of the spawned object.
+@export var _collision_check_radius: float = 100.0
 
 var _time: float = 0.0
 var _min_distance_squared
-## Used to see if the randomly generated position to spawn a lootbox is colliding with a map obstacle.
+## Used to see if the randomly generated position to spawn a object is colliding with a map obstacle.
 var _spawn_collision_check_params: PhysicsShapeQueryParameters2D = PhysicsShapeQueryParameters2D.new()
 
 
 func _ready() -> void:
 	super()
 	
-	_min_distance_squared = MIN_DISTANCE_FROM_NEAREST_PLAYER * MIN_DISTANCE_FROM_NEAREST_PLAYER
+	_min_distance_squared = _min_distance_from_nearest_player ** 2
 	
 	_spawn_collision_check_params.collide_with_areas = false
 	_spawn_collision_check_params.collide_with_bodies = true
 	# The 8th layer/7th bit is the "obstacle" collision layer
 	_spawn_collision_check_params.collision_mask = 1 << 7
 	var circle_shape: CircleShape2D = CircleShape2D.new()
-	circle_shape.radius = COLLISION_CHECK_RADIUS
+	circle_shape.radius = _collision_check_radius
 	_spawn_collision_check_params.shape = circle_shape
 
 
@@ -40,10 +43,10 @@ func _process(delta: float) -> void:
 	
 	_time += delta
 	if _time >= spawn_interval:
-		# Attempt to spawn a LootBox at a random position, but not too close to any player.
+		# Attempt to spawn a object at a random position, but not too close to any player.
 		# Although rare, we limit the amount of random positions generated this frame so that the game doens't 
 		# hang for a while in case we keep generating bad positions. In this case, we wait until next frame to 
-		# try spawning the LootBox again.
+		# try spawning the object again.
 		var found_good_position = false
 		var spawn_pos = Vector2.ZERO
 		var checks = 0
@@ -71,13 +74,13 @@ func _process(delta: float) -> void:
 			checks += 1
 		
 		if found_good_position:
-			# We found a valid position before running out of checks this frame, so spawn the LootBox.
-			var loot_box: LootBox = loot_box_scene.instantiate()
-			get_node("..").add_child(loot_box, true)
-			loot_box.teleport.rpc(spawn_pos)
+			# We found a valid position before running out of checks this frame, so spawn the object.
+			var spawned_node: Node2D = spawn_object_scene.instantiate()
+			get_node("..").add_child(spawned_node, true)
+			spawned_node.teleport.rpc(spawn_pos)
 			_time = 0.0
 
 
-## Do not call. Does nothing. LootBox is spawned in _process rather than through the spawn() function.
+## Do not call. Does nothing. Object is spawned in _process rather than through the spawn() function.
 func spawn(_scene_to_spawn: PackedScene) -> Node2D:
 	return null
