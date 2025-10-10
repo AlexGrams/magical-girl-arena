41ae60bcf955ae6731e56b88e28ca62be948cca0
Victoria W <56736494+Tori-W@users.noreply.github.com>
2025-10-10 12:18:10 -0700

    Added Corvus wing flapping SFX

---
+++ UI/corvus_animated_sprite.gd
@@ -0,0 +1,9 @@
+extends Sprite2D
+## Used to call SFX for Corvus's idle animation (flapping wings)
+
+## Boolean is needed so that SFX does not playing during spawn in
+@export var is_playing: bool = false
+
+func play_flap_sfx():
+	if is_playing:
+		AudioManager.create_audio_at_location(global_position, SoundEffectSettings.SOUND_EFFECT_TYPE.CORVUS_FLAP_WING)
