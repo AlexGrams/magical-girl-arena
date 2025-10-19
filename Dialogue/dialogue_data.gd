02dbc784be95ea5ee93889534392cba2a9f0ae3f
Alexander Grams <alexgrams222@gmail.com>
2025-10-19 02:26:52 -0700

    DialogueData can add conditions for characters that have no lines but must also be present.

--- Dialogue/dialogue_data.gd
+++ Dialogue/dialogue_data.gd
@@ -7,11 +7,13 @@ extends Resource
 @export var lines: Array[DialogueLine] = []
 ## At what point in the game can this dialogue be played. 
 @export var play_conditions: Array[Constants.DialoguePlayCondition] = []
+## Characters that have no lines but must also be present in order to play this dialogue.
+@export var _extra_characters: Array[Constants.Character] = []
 
 
 ## Returns list of characters that participate in this dialogue.
 func get_characters() -> Array[Constants.Character]:
-	var result: Array[Constants.Character] = [] 
+	var result: Array[Constants.Character] = _extra_characters
 	for line: DialogueLine in lines:
 		if line.speaker not in result:
 			result.append(line.speaker)
