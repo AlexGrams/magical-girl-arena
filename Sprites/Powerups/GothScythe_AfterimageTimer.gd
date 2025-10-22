c89ad570749a71309c43e026e35d5a7fae5943be
Alexander Grams <alexgrams222@gmail.com>
2025-10-22 15:10:43 -0700

    Removed files not related to scripts.

--- Sprites/Powerups/GothScythe_AfterimageTimer.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite2D
-
-var time_elapsed = 0;
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	time_elapsed += delta
-	if (time_elapsed > 1):
-		queue_free()
--- Sprites/Powerups/paintbrush_butterfly_sprite.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var animation_player = $AnimationPlayer
-	var animation_length: float = animation_player.get_animation("wing flap").length
-	animation_player.play("wing flap")
-	animation_player.seek(randf_range(0.0, animation_length))
